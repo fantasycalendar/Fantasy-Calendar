2eca5f00373367dd67370842ad418295d06dc57e
V13Axel <alex@stechstudio.com>
2019-08-08 21:13:46 -0400

    automatically clean up old auth tokens

---
+++ app/Authtoken.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+use Carbon\Carbon;
+
+class Authtoken extends Model
+{
+    protected $table = 'auth_tokens';
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
+
+    public function scopeIsExpired($query) {
+        return $query->where('expires', '<', Carbon::now());
+    }
+}
