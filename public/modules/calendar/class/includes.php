3c7e33f54005b3334d24ae6eb1fae4983ad1c0c4
V13Axel <alex@stechstudio.com>
2019-08-08 20:47:13 -0400

    remove c_calendar.php

--- public/modules/calendar/class/includes.php
+++ public/modules/calendar/class/includes.php
@@ -5,6 +5,5 @@ require_once __DIR__ . '/../../../../vendor/autoload.php';
 require_once 'c_db.php';
 require_once 'c_user.php';
 require_once 'c_auth_token.php';
-require_once 'c_calendar.php';
 
 ?>
