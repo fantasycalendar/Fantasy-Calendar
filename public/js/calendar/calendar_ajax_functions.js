4cb8efb1c3856efc08820923b49328b980e1e6c2
V13Axel <alex@stechstudio.com>
2019-08-10 17:44:39 -0400

    get rid of create event ajax function

--- public/js/calendar/calendar_ajax_functions.js
+++ public/js/calendar/calendar_ajax_functions.js
@@ -379,30 +379,6 @@ function create_calendar(){
 
 }
 
-function create_event_category(list, category, callback) {
-	// event = Object.assign({_method: 'store'}, event);
-
-	if(static_data.event_data.categories.length < 1) {
-		static_data.event_data.categories = {};
-	}
-
-	$.ajax({
-		url: window.baseurl+"api/eventcategory",
-		type: "post",
-		dataType: "json",
-		data: category,
-		success: function ( result ) {
-			static_data.event_data.categories[result.id] = result;
-			repopulate_event_category_lists();
-			callback(list, result.id, result);
-		},
-		error: function ( log ) {
-			console.log(log);
-		}
-	});
-}
-
-
 function get_event_comments(event_id, callback){
 
 	$.ajax({
