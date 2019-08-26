425a51f36a398d35a2fef67a3fdb710213a6a43d
Haxxer <adamoresten@gmail.com>
2019-08-26 19:24:03 +0100

    Added new buttons to edit page

--- resources/views/inputs/sidebar/edit.blade.php
+++ resources/views/inputs/sidebar/edit.blade.php
@@ -14,7 +14,24 @@
 
     <div class='wrap-collapsible margin-below'>
         <button type="button" disabled id='btn_save' class='btn btn-lg btn-success btn-block'>No changes to save</button>
-
-        <button type="button" id='btn_delete' class='btn btn-sm btn-danger btn-block'>Delete</button>
     </div>
+
+    <div class='wrap-collapsible'>
+        <div class='detail-row'>
+            <div class='detail-column third'>
+                <a href="view" class='full'>
+                    <button type="button" class='btn btn-sm btn-info btn-block'>Go to view</button>
+                </a>
+            </div>
+            <div class='detail-column third'>
+                <a href="export" target="_blank" class='full'>
+                    <button type="button" class='btn btn-sm btn-primary btn-block'>Export</button>
+                </a>
+            </div>
+            <div class='detail-column third'>
+                <button type="button" id='btn_delete' class='btn btn-sm btn-danger btn-block'>Delete</button>
+            </div>
+        </div>
+    </div>
+
 @endsection
