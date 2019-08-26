c9894a7e74f5fb1cf9708c2098f54a2d24bdfdc6
V13Axel <alex@stechstudio.com>
2019-08-25 21:50:43 -0400

    we're eventually gonna have one of these, might as well make the links work when it happens

--- resources/views/templates/footnote.blade.php
+++ resources/views/templates/footnote.blade.php
@@ -1,1 +1,1 @@
-<footer id="footer">fantasy-calendar.com version {{ env("APP_VERSION") }} - Copyright © 2018-{{ date("Y") }} Fantasy Calendar Ltd. — <a href="privacypolicy">Privacy Policy</a> — <a href="privacypolicy">Contact Us</a>
+<footer id="footer">fantasy-calendar.com version {{ env("APP_VERSION") }} - Copyright © 2018-{{ date("Y") }} Fantasy Calendar Ltd. — <a href="/privacypolicy">Privacy Policy</a> — <a href="/privacypolicy">Contact Us</a>
