b4273e87222a580735d9b009cbdb93295a22c2e8
V13Axel <alex@stechstudio.com>
2019-08-31 13:51:45 -0400

    import and use fontawesome, visual edits to homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,42 +19,44 @@
 @endpush
 
 @section('content')
-    <div class='detail-row'>
-
-        <div class='detail-column @isset($changelog) half @else full @endisset'>
-            <div class='index_container'>
-                @isset($calendars)
-                    <div id='user_calendar_list'>
+    <div class="container">
+        @isset($calendars)
+                <div class='row'>
                     @foreach($calendars as $calendar)
-                        <div class='user_calendar @if(!isset($changelog)) quarter @endif'>
-                            <div class='name'>
-                                <b>{!! $calendar->name !!}</b><br>by {{ $calendar->user->username }}
-                            </div>
-
-                            <div class='icon_container'>
-                                <a class='image_link' href='{{ route('calendars.edit', ['calendar'=> $calendar->hash ]) }}'>
-                                    <img class='icon' src='{{ asset("resources/icons/edit_icon.png") }}' title="Edit"/>
-                                </a>
+                        <div class="col col-sm-6 col-md-4 col-lg-3">
+                            <div class='user_calendar card text-center'>
+                                <div class="card-header">
+                                    <h5 class="calendar__list-name">{!! $calendar->name !!}</h5>
+                                    <span class="calendar__list-username">{{ $calendar->user->username }}</span>
+                                </div>
+                                <div class="card-body">
+                                    <div class='icon_container'>
+                                        <a class='calendar_action' href='{{ route('calendars.edit', ['id'=> $calendar->hash ]) }}'>
+                                            <i class="fa fa-edit"></i>
+                                        </a>
+                                        <a class='calendar_action' href='{{ route('calendars.show', ['id'=> $calendar->hash ]) }}'>
+                                            <i class="fa fa-eye"></i>
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     @endforeach
-                    </div>
-                @endisset
-            </div>
-
-        </div>
+                </div>
+        @endisset
 
         @isset($changelog)
-            <div class='detail-column half'>
-                <div id='changelog'>
-                    <h2>Changelog</h2>
+            <div class="detail-row">
+                <div class='detail-column full'>
+                    <div id='changelog'>
+                        <h2>Changelog</h2>
 
-                    <div class="changelog__content">
-                    {!! $changelog !!}
+                        <div class="changelog__content">
+                        {!! $changelog !!}
+                        </div>
                     </div>
                 </div>
             </div>
         @endisset
-
     </div>
 @endsection
