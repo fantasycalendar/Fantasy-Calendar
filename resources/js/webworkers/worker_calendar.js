fbda84b81215cbb62fceb8cff8215f3b267cff8d
Haxxer <adamoresten@gmail.com>
2019-11-29 23:37:53 +0000

    Forgot important function calls - oops

--- resources/js/webworkers/worker_calendar.js
+++ resources/js/webworkers/worker_calendar.js
@@ -984,6 +984,7 @@ var calendar_builder = {
 		end_epoch = epoch;
 
 		climate_generator = new Climate(this.data.epochs, this.static_data, this.dynamic_data, calendar_start_epoch, calendar_end_epoch);
+		this.data.epochs = climate_generator.generate()
 		
 		return {
 			epoch_data: this.data.epochs,
@@ -1980,7 +1981,7 @@ var calendar_builder = {
 		}
 
 		climate_generator = new Climate(this.data.epochs, this.static_data, this.dynamic_data, calendar_start_epoch, calendar_end_epoch);
-		climate_generator.generate();
+		this.data.epochs = climate_generator.generate();
 
 		if(!this.static_data.settings.show_current_month){
 			year_day = 1;
