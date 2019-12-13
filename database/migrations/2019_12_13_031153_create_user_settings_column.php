a7183db894c09ea02087bde3513fa7c02befa3dc
V13Axel <alex@stechstudio.com>
2019-12-13 13:21:28 -0500

    fixes

--- database/migrations/2019_12_13_031153_create_user_settings_column.php
+++ database/migrations/2019_12_13_031153_create_user_settings_column.php
@@ -14,7 +14,7 @@ class CreateUserSettingsColumn extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->json('settings');
+            $table->string('settings');
         });
     }
 
