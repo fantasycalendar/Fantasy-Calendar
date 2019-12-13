86053235e4de1853241133c5b81a152b08562e38
V13Axel <alex@stechstudio.com>
2019-12-12 23:15:22 -0500

    Functional setting toggle

--- resources/views/pages/settings.blade.php
+++ resources/views/pages/settings.blade.php
@@ -1,5 +1,18 @@
 @extends('templates._page')
 
+@push('head')
+    <script>
+        function toggleSetting(name) {
+            document.getElementById(name).value=1-document.getElementById(name).value;
+            if(document.getElementById(name+'_input').getAttribute('checked') === 'checked') {
+                document.getElementById(name+'_input').removeAttribute('checked');
+            } else {
+                document.getElementById(name+'_input').setAttribute('checked', 'checked');
+            }
+        }
+    </script>
+@endpush
+
 @section('content')
     <div class="container">
         <div class="row">
@@ -8,16 +21,18 @@
                     <h1 class="card-header">User settings</h1>
                     <div class="card-body">
                         <div class="card-text p-4">
-                            <form action="/settings" method="post">
+                            <form id="settings" method="post">
                                 @csrf
 
-                                <div class="form-check pb-2">
-                                    <input type="checkbox" name="dark_theme" class="form-check-input" id="dark_theme">
+                                <div class="form-check pb-2" onclick="toggleSetting('dark_theme')">
+                                    <input id="dark_theme" type="hidden" name="dark_theme" @if($settings['dark_theme']) value="1" @else value="0" @endif>
+                                    <input id="dark_theme_input" type="checkbox" class="form-check-input" id="dark_theme" @if($settings['dark_theme']) checked="checked" @endif>
                                     <label class="form-check-label" for="dark_theme">Enable dark theme</label>
                                 </div>
 
-                                <button type="submit" class="btn btn-primary">Save Settings</button>
+                                <button class="btn btn-primary">Save Settings</button>
                             </form>
+
                             @foreach($settings as $setting => $value)
                                 <p>{{ $setting }}: {{ $value }}</p>
                             @endforeach
