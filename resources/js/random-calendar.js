84ae30897044ada4a412fa5e1c78cd5bcb253a3b
Haxxer <adamoresten@gmail.com>
2019-12-25 14:15:23 +0000

    Randomizer respects longest moon cycle for month lengths

--- resources/js/random-calendar.js
+++ resources/js/random-calendar.js
@@ -1,70 +1,87 @@
 class RandomCalendar{
 
-	rndUNorm(idx){
-		return fract(43758.5453 * Math.sin(this.seed + (78.233 * idx)));
+	get rndUNorm(){
+		this.idx++;
+		return fract(43758.5453 * Math.sin(this.seed + (78.233 * this.idx)));
 	}
 
-	random_int_between(idx, min, max){
-		return Math.round(this.rndUNorm(idx) * (max - min) + min);
+	random_int_between(min, max){
+		return Math.round(this.rndUNorm * (max - min) + min);
 	}
 
 	randomize(static_data){
 
 		this.seed = Math.abs((Math.random().toString().substr(7)|0));
+		this.idx = 0;
 
-		var step = 0;
+		var weekdays	= this.random_int_between(5, 10);
+		var months		= this.random_int_between(6, 18);
+		var leap_days	= this.random_int_between(0, 3);
+		var moons		= this.random_int_between(1, 5);
 
-		var weekdays	= this.random_int_between(step, 5, 10);
-		step++;
-		var months		= this.random_int_between(step, 6, 18);
-		step++;
-		var leap_days	= this.random_int_between(step, 0, 3);
-		step++;
-		var moons		= this.random_int_between(step, 0, 5);
-		step++;
+		var longest_moon_cycle = 0
+		var longest_moon_index = 0
+
+		static_data.moons = [];
+		for(var i = 0; i < moons; i++){
+
+			var cycle = this.random_int_between(4, 48)
+
+			static_data.moons.push({
+				'name':  `Moon ${i+1}`,
+				'cycle': cycle,
+				'shift': this.random_int_between(4, 48),
+				'granularity': get_moon_granularity(cycle),
+				'color': '#FFFFFF',
+				'hidden': false,
+				'custom_phase': false
+			});
+
+			if(cycle > longest_moon_cycle){
+				longest_moon_cycle = cycle;
+				longest_moon_index = i;
+			}
+		}
+
+		static_data.moons[longest_moon_index].shift = 0;
 
 		static_data.year_data.global_week = [];
-		for(var i = 0; i < weekdays-1; i++){
+		for(var i = 0; i < weekdays; i++){
 			static_data.year_data.global_week.push(`Weekday ${i+1}`)			
 		}
 
+		var year_length = 0;
 		static_data.year_data.timespans = [];
-		for(var i = 0; i < months-1; i++, step++){
+		for(var i = 0; i < months; i++){
+			var length = this.random_int_between(longest_moon_cycle-2, longest_moon_cycle+2);
+			year_length += length;
 			static_data.year_data.timespans.push({
 				"name": `Month ${i+1}`,
 				'type': 'month',
 				'interval': 1,
 				'offset': 0,
-				'length': this.random_int_between(step, 20, 50)
+				'length': length
 			});
 		}
 
 		static_data.year_data.leap_days = [];
-		for(var i = 0; i < leap_days-1; i++, step++){
+		for(var i = 0; i < leap_days; i++){
+			var interval = this.random_int_between(2, 8);
+			year_length += 1/interval;
 			static_data.year_data.leap_days.push({
 				'name': `Leap day ${i+1}`,
 				'intercalary': false,
-				'timespan': this.random_int_between(step+1000, 0, months-1),
+				'timespan': this.random_int_between(0, months-1),
 				'adds_week_day': false,
 				'day': 0,
 				'week_day': '',
-				'interval': this.random_int_between(step+100, 2, 8).toString(),
+				'interval': interval.toString(),
 				'offset': 0
 			});
 		}
 
-		static_data.moons = [];
-		for(var i = 0; i < moons-1; i++, step++){
-			static_data.moons.push({
-				'name':  `Moon ${i+1}`,
-				'cycle': this.random_int_between(step+100, 4, 48),
-				'shift': this.random_int_between(step+1000, 4, 48),
-				'granularity': get_moon_granularity(this.random_int_between(step+100, 4, 48)),
-				'color': '#FFFFFF',
-				'hidden': false,
-				'custom_phase': false
-			});
-		}
+		static_data.moons[longest_moon_index].cycle = year_length/months;
+		static_data.moons[longest_moon_index].granularity = get_moon_granularity(year_length/months);
 
 		return static_data;
 
