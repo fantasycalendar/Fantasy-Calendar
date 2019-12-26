dd6524009f0092503b1d67e891e58af9930ab0a9
Haxxer <adamoresten@gmail.com>
2019-12-26 17:47:28 +0000

    Fixed swal merge

--- resources/views/calendar/create.blade.php
+++ resources/views/calendar/create.blade.php
@@ -127,12 +127,14 @@
                     }
                 }else if($('#presets').val() == 'Random'){
 
-                    swal({
+                    swal.fire({
                         title: "Are you sure?",
                         text: `This will randomly generate new weekdays, months, leap days, moons, and seasons which will override what you have, are you sure you want to do this?`,
-                        buttons: true,
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Generate',
                         icon: "warning",
-                        dangerMode: true,
                     })
                     .then((generate) => {
                         if(generate) {
