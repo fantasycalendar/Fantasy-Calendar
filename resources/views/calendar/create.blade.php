f2efc4d7a0b5f770545e9e5c6619498a22dd6ada
Haxxer <adamoresten@gmail.com>
2019-12-26 08:39:30 +0000

    Adjusted message

--- resources/views/calendar/create.blade.php
+++ resources/views/calendar/create.blade.php
@@ -129,7 +129,7 @@
 
                     swal({
                         title: "Are you sure?",
-                        text: `This will randomly generate new weekdays, months, leap days, and moons which will override what you have, are you sure you want to do this?`,
+                        text: `This will randomly generate new weekdays, months, leap days, moons, and seasons which will override what you have, are you sure you want to do this?`,
                         buttons: true,
                         icon: "warning",
                         dangerMode: true,
