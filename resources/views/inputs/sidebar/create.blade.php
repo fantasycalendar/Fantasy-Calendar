84f0cb41eb860527ec0232f6c6baac71e06a8f24
Haxxer <adamoresten@gmail.com>
2020-01-01 23:16:56 +0000

    Initial commit - changes on input column

--- resources/views/inputs/sidebar/create.blade.php
+++ resources/views/inputs/sidebar/create.blade.php
@@ -5,31 +5,43 @@
         <div class='title-text center-text'>Create Calendar</div>
     </div>
 
-
-    <div class='wrap-collapsible'>
-        <div class='detail-row form-inline'>
-            <input type='text' class='form-control form-control-lg full' id='calendar_name' placeholder='Calendar name' />
+    <div class='wrap-collapsible content'>
+        <div class='col-12 mb-1'>
+            <div class='row'>
+                <input type='text' class='form-control form-control-lg full' id='calendar_name' placeholder='Calendar name' />
+            </div>
         </div>
     </div>
 
+    <div class='wrap-collapsible content'>
+        <div class='col-12 mb-1'>
+            <div class='row'>
+                @if(Auth::check())
+                    <button type="button" disabled id='btn_create' class='btn btn-lg btn-danger btn-block'>Cannot create yet</button>
+                @else
+                    <button type='button' class='login-show-button btn btn-lg btn-info btn-block'>Log in to save</button>
+                @endif
+            </div>
+        </div>
+    </div>
 
-    <div class='wrap-collapsible margin-below'>
+    <div id='preset_container' class='d-flex px-3 mb-1'>
 
-    @if(Auth::check())
-        <button type="button" disabled id='btn_create' class='btn btn-lg btn-danger btn-block'>Cannot create yet</button>
-    @else
-        <button type='button' class='login-show-button btn btn-lg btn-info btn-block'>Log in to save</button>
-    @endif
+        <div class='flex-column flex-grow-1'>
 
-    <div id='preset_container'>
+            <select class='form-control form-control-sm' id='presets'>
+                <option>Presets</option>
+                <option>Custom JSON</option>
+                <option>Random</option>
+            </select>
 
-        <select class='form-control' id='presets'>
-            <option>Presets</option>
-            <option>Custom JSON</option>
-            <option>Random</option>
-        </select>
+        </div>
+
+        <div class='flex-column pl-1'>
         
-        <button id='json_apply' disabled type='button' class='btn btn-warning btn-sm' >Apply</button>
+            <button id='json_apply' disabled type='button' class='btn btn-sm btn-warning full m-0' >Apply</button>
+
+        </div>
 
     </div>
 
@@ -37,5 +49,5 @@
         <div>JSON input:</div>
         <textarea class='form-control' id='json_input'></textarea>
     </div>
-</div>
+
 @endsection
--- resources/views/inputs/sidebar/edit.blade.php
+++ resources/views/inputs/sidebar/edit.blade.php
@@ -5,29 +5,36 @@
         <div class='title-text center-text'>Edit Calendar</div>
     </div>
 
-    <div class='wrap-collapsible'>
-        <div class='detail-row'>
-            <input type='text' class='form-control form-control-lg full' id='calendar_name' placeholder='Calendar name' />
+    <div class='wrap-collapsible content'>
+        <div class='col-12 mb-1'>
+            <div class='row'>
+                <input type='text' class='form-control form-control-lg full' id='calendar_name' placeholder='Calendar name' />
+            </div>
         </div>
     </div>
 
-    <div class='wrap-collapsible margin-below'>
-        <button type="button" disabled id='btn_save' class='btn btn-lg btn-success btn-block'>No changes to save</button>
+    <div class='wrap-collapsible content'>
+        <div class='col-12 mb-1'>
+            <div class='row'>
+                <button type="button" disabled id='btn_save' class='btn btn-lg btn-success btn-block'>No changes to save</button>
+            </div>
+        </div>
     </div>
 
-    <div class='wrap-collapsible'>
-        <div class='detail-row'>
-            <div class='detail-column third'>
+    <div class='wrap-collapsible content'>
+
+        <div class='row'>
+            <div class='col-4 mb-1 pr-0'>
                 <a href="{{ route('calendars.show', ['calendar'=> $calendar->hash ]) }}" class='full'>
                     <button type="button" class='btn btn-sm btn-info btn-block'>View Mode</button>
                 </a>
             </div>
-            <div class='detail-column third'>
+            <div class='col-4 px-2 mb-1'>
                 <a href="export" target="_blank" class='full'>
                     <button type="button" class='btn btn-sm btn-primary btn-block'>Export</button>
                 </a>
             </div>
-            <div class='detail-column third'>
+            <div class='col-4 mb-1 pl-0'>
                 <button type="button" id='btn_delete' class='btn btn-sm btn-danger btn-block'>Delete</button>
             </div>
         </div>
