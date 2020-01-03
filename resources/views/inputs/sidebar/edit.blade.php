3747ed3e08f2edba62bcf43cc90893d4a6f5bfdb
V13Axel <alex@stechstudio.com>
2020-01-02 22:46:42 -0500

    more consistent spacing and nicer looking view/export/delete buttons

--- resources/views/inputs/sidebar/edit.blade.php
+++ resources/views/inputs/sidebar/edit.blade.php
@@ -6,7 +6,7 @@
     </div>
 
     <div class='wrap-collapsible content'>
-        <div class='col-12 mb-1'>
+        <div class='col-12 mb-2'>
             <div class='row'>
                 <input type='text' class='form-control form-control-lg full' id='calendar_name' placeholder='Calendar name' />
             </div>
@@ -14,29 +14,22 @@
     </div>
 
     <div class='wrap-collapsible content'>
-        <div class='col-12 mb-1'>
+        <div class='col-12 my-2'>
             <div class='row'>
                 <button type="button" disabled id='btn_save' class='btn btn-lg btn-success btn-block'>No changes to save</button>
             </div>
         </div>
     </div>
 
-    <div class='wrap-collapsible content'>
-
-        <div class='row'>
-            <div class='col-4 mb-1 pr-0'>
-                <a href="{{ route('calendars.show', ['calendar'=> $calendar->hash ]) }}" class='full'>
-                    <button type="button" class='btn btn-sm btn-info btn-block'>View Mode</button>
-                </a>
-            </div>
-            <div class='col-4 px-2 mb-1'>
-                <a href="export" target="_blank" class='full'>
-                    <button type="button" class='btn btn-sm btn-primary btn-block'>Export</button>
-                </a>
-            </div>
-            <div class='col-4 mb-1 pl-0'>
-                <button type="button" id='btn_delete' class='btn btn-sm btn-danger btn-block'>Delete</button>
-            </div>
+    <div class='wrap-collapsible'>
+        <div class='d-flex my-2 w-100'>
+            <a href="{{ route('calendars.show', ['calendar'=> $calendar->hash ]) }}" class="btn w-100 btn-sm btn-info">
+                View Mode
+            </a>
+            <a href="export" target="_blank" class="btn w-100 btn-sm btn-primary mx-2">
+                Export
+            </a>
+            <button type="button" id='btn_delete' class='btn w-100 btn-sm btn-danger btn-block'>Delete</button>
         </div>
     </div>
 
