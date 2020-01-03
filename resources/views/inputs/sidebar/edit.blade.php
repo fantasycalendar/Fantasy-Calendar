e47d1eab85175736e2b190b5d5578522edbb5a70
V13Axel <alex@stechstudio.com>
2020-01-02 22:47:10 -0500

    remove the word 'mode', since action is already implied

--- resources/views/inputs/sidebar/edit.blade.php
+++ resources/views/inputs/sidebar/edit.blade.php
@@ -24,7 +24,7 @@
     <div class='wrap-collapsible'>
         <div class='d-flex my-2 w-100'>
             <a href="{{ route('calendars.show', ['calendar'=> $calendar->hash ]) }}" class="btn w-100 btn-sm btn-info">
-                View Mode
+                View
             </a>
             <a href="export" target="_blank" class="btn w-100 btn-sm btn-primary mx-2">
                 Export
