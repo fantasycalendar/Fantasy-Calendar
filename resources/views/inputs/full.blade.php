2ef9651963efb1096382709823508c445f75d76f
Haxxer <adamoresten@gmail.com>
2020-01-13 11:47:49 +0000

    Minor margin changes

--- resources/views/inputs/full.blade.php
+++ resources/views/inputs/full.blade.php
@@ -744,11 +744,11 @@
 				<div class='row no-gutters bold-text'>
 					Cycle format:
 				</div>
-				<div class="row no-gutters mb-2">
+				<div class="row no-gutters">
 					<input type='text' id='cycle_format' class='form-control name static_input protip' data='cycles' fc-index='format' placeholder='Cycle &lcub;&lcub;1&rcub;&rcub;' data-pt-position="right" data-pt-title="This is the template for the cycles you have. Each cycle part has a set of names which you can add to the top of the calendar. Add one with this field empty to see how this works!">
 				</div>
 
-				<div class='row no-gutters my-3'>
+				<div class='row no-gutters my-2'>
 					<div class='separator'></div>
 				</div>
 
