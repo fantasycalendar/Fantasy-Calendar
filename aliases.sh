420b03a2d03d5c798a949859270d3d150913cd0a
V13Axel <alex@stechstudio.com>
2020-01-31 00:27:24 -0500

    artisan alias

--- aliases.sh
+++ aliases.sh
@@ -1,2 +1,3 @@
 alias composer='docker run -it -v ${PWD}/:/app composer'
 alias npm='docker run -it -v ${PWD}/:/app -w /app node npm'
+alias artisan='docker run -it -v ${PWD}/:/fantasy-calendar -w /fantasy-calendar fc-php php artisan'
