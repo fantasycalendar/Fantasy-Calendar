d9e92d454c503bbfe5c222e9b88df492ac85c736
V13Axel <alex@stechstudio.com>
2020-07-20 17:32:17 -0400

    make homepage a little lighter

--- resources/views/templates/_page.blade.php
+++ resources/views/templates/_page.blade.php
@@ -2,9 +2,68 @@
 
 <html lang="en" prefix="og: http://ogp.me/ns#">
 
-    @include('templates._head_content')
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="description" content="All-in-One Fantasy Calendar Generator - Creation of calendars and time-tracking in your homebrew or pre-made campaign worlds have never been easier!">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-	<body class="page-{{ str_replace('.', '-', Route::currentRouteName()) }} @stack('page-class')">
+    <meta property="og:title" content="{{ $calendar->name ?? $title ?? "Fantasy Calendar" }}">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="{{ url()->full() }}">
+    <meta property="og:image" content="{{ url('/resources/discord_logo.jpg') }}">
+
+    <title>
+        Welcome to Fantasy Calendar
+    </title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/resources/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/resources/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/resources/favicon-16x16.png">
+    <link rel="manifest" href="/resources/site.webmanifest">
+    <link rel="mask-icon" href="/resources/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+    <script src="{{ mix('/js/app.js') }}"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.17.0/dist/jquery.validate.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
+    <script>
+        $(document).ready(function(){
+            window.baseurl = '{{ getenv('WEBADDRESS') }}';
+            window.apiurl = '{{ getenv('WEBADDRESS') }}'+'api';
+
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
+                    'Authorization': 'Bearer '+$('meta[name="api-token"]').attr('content')
+                }
+            });
+        });
+    </script>
+
+    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+    <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/3.2.1/css/font-awesome.min.css">
+
+    <script src="/js/login.js"></script>
+
+    <script src="{{ mix('/js/calendar/header.js') }}"></script>
+
+    @if(Auth::check() && Auth::user()->setting('dark_theme') && !request()->is('/'))
+        <link rel="stylesheet" href="{{ mix('/css/app-dark.css') }}">
+    @else
+        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    @endif
+
+    @stack('head')
+</head>
+
+
+<body class="page-{{ str_replace('.', '-', Route::currentRouteName()) }} @stack('page-class')">
 
 		@include('templates._header')
 		<div id="content">
