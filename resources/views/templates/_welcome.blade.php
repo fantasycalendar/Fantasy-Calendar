c927f8dba7b6c696daa5d604a3d9ffca5f84fb9d
V13Axel <alex@stechstudio.com>
2020-07-28 23:35:56 -0400

    quick and dirty, get the menu working on the homepage

--- resources/views/templates/_welcome.blade.php
+++ resources/views/templates/_welcome.blade.php
@@ -29,11 +29,11 @@
     <meta name="msapplication-config" content="{{ asset("/resources/browserconfig.xml") }}">
     <meta name="theme-color" content="#2f855a">
 
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+
 
     <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
     <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
-
-    <script src="{{ mix('js/calendar/header.js') }}"></script>
 
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
@@ -43,7 +43,83 @@
 
 <body class="page-welcome">
 
-@include('templates._header')
+<nav class="navbar navbar-expand-lg navbar-dark bg-accent" x-data="{ open: false }">
+    <a class="navbar-brand" href="{{ route('home') }}">
+        <img class="navbar-logo mr-2" src="{{ asset('resources/header_logo.png') }}">
+        Fantasy Calendar
+    </a>
+    <button class="navbar-toggler" type="button" @click="open = !open" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse show" x-show="open" id="collapsemenu">
+
+        <ul class="navbar-nav mr-auto">
+            @auth
+                <li class="nav-item my-calendars">
+                    <a class="nav-link" href="{{ route('calendars.index') }}">My Calendars</a>
+                </li>
+            @endauth
+            <li class="nav-item new-calendar">
+                <a class="nav-link" href="{{ route('calendars.create') }}">New Calendar</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav">
+            @auth
+                @if(Auth::user()->isAdmin())
+                    <li class="nav-item"><a href="{{ route('code16.sharp.home') }}" class="nav-link">Admin Panel</a></li>
+                @endif
+                <li class="nav-item"><a href="{{ route('settings') }}" class="nav-link">Settings</a></li>
+                <li class="nav-item"><a href="/profile" class="nav-link">Profile</a></li>
+                @unless(request()->is('/calendar/*'))
+                    <li class="nav-item"><a href="{{ route('logout') }}" class="nav-link">Logout</a></li>
+                @else
+                    <li class="nav-item"><a href="javascript:" id="logout-button" class="nav-link">Logout</a></li>
+                @endunless
+            @else
+                @unless(request()->is('/calendar/*'))
+                    <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Login</a></li>
+                @else
+                    <li class="nav-item"><a href="javascript:" id="login-show-button" class="nav-link">Login</a></li>
+                @endunless
+
+                <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">Register</a></li>
+            @endauth
+
+        </ul>
+    </div>
+</nav>
+
+<div id="alert_background">
+    <div id="alert">
+        <span id="alert_closebtn" onclick="this.parentElement.parentElement.style.display='none';">&times;</span>
+        <span id="alert_text">
+			This is an alert box.
+		</span>
+    </div>
+</div>
+
+@guest
+    <div id="login-background" class="basic-background blurred_background">
+        <div class="basic-container">
+            <div class="basic-wrapper">
+                <form id="login-form" class="basic-form" method="POST">
+                    <h3 class="basic-form-heading">Login</h3>
+                    <input type="text" class="form-control" id="login_identity" name="identity" placeholder="Username or E-mail" autofocus="" autocomplete="username" />
+                    <input type="password" class="form-control" id="login_password" name="password" placeholder="Password" autocomplete="current-password" />
+                    <div id="login_messagebox"></div>
+                    <label class="form-control checkbox">
+                        <input type="checkbox" value="remember-me" id="login_rememberMe" name="rememberMe"> Remember me
+                    </label>
+                    <button class="btn btn-lg btn-primary btn-block mt-2" id="login_button" type="submit">Login</button>
+                    <a href="/password/reset"><button class="btn btn-sm btn-secondary btn-block" id="forgotten_password" type="button">Forgotten Password</button></a>
+                </form>
+            </div>
+        </div>
+    </div>
+@endguest
+
+
 <div id="content">
     @yield('content')
 </div>
