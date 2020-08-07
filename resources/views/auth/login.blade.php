425ccb1a29b044594e473f9dceba8a091058c666
V13Axel <alex@stechstudio.com>
2020-08-06 23:36:16 -0400

    this should have an ID

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -59,7 +59,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button id="login" type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
 
