03c2eabf5f72fed422db9322de9ac93b0f0c60e2
V13Axel <alex@stechstudio.com>
2020-08-21 00:21:42 -0400

    things look funny with placeholder images.

--- resources/views/pages/whats-new.blade.php
+++ resources/views/pages/whats-new.blade.php
@@ -25,7 +25,7 @@
             color: #246645;
         }
 
-        div.section {
+        div.fullheight {
             height: 100vh;
         }
 
@@ -66,13 +66,13 @@
 
 @section('content')
     <div>
-        <div class="section green-border welcome" style="background-color: #edf2f7; background-image: url({{ asset('resources/whats-new-angle-blur-transparent.png') }}); background-size: cover; background-position: center center; background-attachment: fixed;">
+        <div class="fullheight green-border welcome" style="background-color: #edf2f7; background-image: url({{ asset('resources/whats-new-angle-blur-transparent.png') }}); background-size: cover; background-position: center center; background-attachment: fixed;">
             <h1>Fantasy Calendar is getting an update!</h1>
             <h2>Here is what you can expect in Fantasy Calendar 2.0.</h2>
-            <h3 class="next-link" style="font-size: 40px; position: absolute; bottom: 10px; width: 100%; text-align: center; z-index: 21;"><a href="#section2"><i class="fa fa-chevron-circle-down"></i> Scroll down to see more <i class="fa fa-chevron-circle-down"></i></a></h3>
+            <h3 class="next-link" style="font-size: 40px; position: absolute; bottom: 10px; width: 100%; text-align: center; z-index: 21;"><a href="#section2"><i class="fa fa-chevron-circle-down"></i> Give it a look <i class="fa fa-chevron-circle-down"></i></a></h3>
         </div>
 
-        <div class="section green-border comparison_image" id="section2" style="background-image: linear-gradient(to right, rgba(255, 255, 255, 0.95), rgba(255, 255, 255, 0.95)), url({{ asset('resources/whats-new-1-0.png') }});">
+        <div class="fullheight green-border comparison_image" id="section2" style="background-image: linear-gradient(to right, rgba(255, 255, 255, 0.95), rgba(255, 255, 255, 0.95)), url({{ asset('resources/whats-new-1-0.png') }});">
             <h1 style="position: absolute; top: 8%; width: 100%; text-align: center; color: #2d3748;">Here's what you're used to.</h1>
             <h2 style="position: absolute; top: 16%; width: 100%; text-align: center; color: #2d3748;">It works great! But I think we can all agree it could use some polish.</h2>
             <img src="{{ asset('/resources/whats-new-1-0.png') }}" style="max-width: 72%; max-height: 50%; box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05); position: absolute;">
@@ -80,7 +80,7 @@
             <div class="overlay"></div>
         </div>
 
-        <div class="section green-border comparison_image" id="section3" style="height: 100vh; background-image: linear-gradient(to right, rgba(255, 255, 255, 0.95), rgba(255, 255, 255, 0.95)), url({{ asset('resources/whats-new-2-0.png') }})">
+        <div class="fullheight green-border comparison_image" id="section3" style="background-image: linear-gradient(to right, rgba(255, 255, 255, 0.95), rgba(255, 255, 255, 0.95)), url({{ asset('resources/whats-new-2-0.png') }})">
             <h1 style="position: absolute; top: 8%; width: 100%; text-align: center; color: #2d3748;">This is Fantasy Calendar 2.0.</h1>
             <h2 style="position: absolute; top: 16%; width: 100%; text-align: center; color: #2d3748;">Lovingly crafted to look good on any screen. Even mobile devices.</h2>
             <h3 class="next-link" style="font-size: 40px; position: absolute; bottom: 10px; width: 100%; text-align: center; z-index: 21;"><a href="#section4"><i class="fa fa-chevron-circle-down"></i> Looks great! Show me more <i class="fa fa-chevron-circle-down"></i></a></h3>
@@ -89,57 +89,47 @@
         </div>
 
         <div class="section comparison_both welcome" id="section4" style="background-color: #edf2f7;">
-            <div class="container">
+            <div class="container my-5 py-5">
                 <div class="row">
-                    <div class="col-12 col-md-4 mt-5">
-                        <h1>A modern look</h1>
-                        <h3>Fantasy Calendar 2.0 looks great!</h3>
+
+                    <div class="col-12 col-md-7 text-center text-md-left">
+                        <img src="{{ asset('resources/whats-new-2-0.png') }}" style="box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);">
                     </div>
 
-                    <div class="col-12 col-md-8 text-right">
+                    <div class="col-12 col-md-5 mt-5 text-center text-md-right">
+                        <h1>A fresh, modern look</h1>
+                        <h3>Looks aren't everything, but they certainly help.</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section">
+            <div class="container my-5 py-5">
+                <div class="row">
+                    <div class="col-12 col-md-6 text-center text-md-left">
+                        <h3>Great on any device</h3>
+                        <h4>For a DM on the go, or for players who like to take notes during the session, Fantasy Calendar 2.0 works great on phones and tablets.</h4>
+                    </div>
+                    <div class="col-12 col-md-6 text-center text-md-right">
+                        {{--                    Placeholder --}}
                         <img src="{{ asset('resources/whats-new-2-0.png') }}" style="box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);">
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="container">
-            <div class="row">
-                <div class="col-12 text-center">
-                    <h1>Of course, looks aren't everything!</h1>
-                    <h2>We also have the features to back it up</h2>
-                </div>
-            </div>
-            <div class="row mt-5">
-                <div class="col-12 col-md-4 text-center">
-                    <h4>Some text</h4>
-                    <ul>
-                        <li>Some text</li>
-                        <li>Some text</li>
-                        <li>Some text</li>
-                        <li>Some text</li>
-                        <li>Some text</li>
-                    </ul>
-                </div>
-                <div class="col-12 col-md-4 text-center">
-                    <h4>Some text</h4>
-                    <ul>
-                        <li>Some text</li>
-                        <li>Some text</li>
-                        <li>Some text</li>
-                        <li>Some text</li>
-                        <li>Some text</li>
-                    </ul>
-                </div>
-                <div class="col-12 col-md-4 text-center">
-                    <h4>Some text</h4>
-                    <ul>
-                        <li>Some text</li>
-                        <li>Some text</li>
-                        <li>Some text</li>
-                        <li>Some text</li>
-                        <li>Some text</li>
-                    </ul>
+        <div class="section darkmode" style="background-color: #222222">
+            <div class="container my-5 py-5">
+                <div class="row">
+                    <div class="col-12 col-md-6 text-center text-md-left">
+                        {{--                    Placeholder --}}
+                        <img src="{{ asset('resources/whats-new-2-0.png') }}" style="box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);">
+                    </div>
+                    <div class="col-12 col-md-6 text-center text-md-right">
+                        <h3 style="color: white;">What lives in the darkness...</h3>
+                        <h4 style="color: white;">That's probably a bit dramatic... But comfort for your retinas is here!</h4>
+                    </div>
                 </div>
             </div>
         </div>
