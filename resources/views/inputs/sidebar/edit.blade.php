348eff969c2db468105725121fd480d712402ed4
Haxxer <adamoresten@gmail.com>
2020-08-22 01:04:07 +0100

    Initial button implementation

--- resources/views/inputs/sidebar/edit.blade.php
+++ resources/views/inputs/sidebar/edit.blade.php
@@ -33,4 +33,38 @@
         </div>
     </div>
 
+    @if(Auth::user()->isAdmin())
+
+        <script>
+            function elevate_to_preset(){
+                swal.fire({
+                    text: "Are you absolutely sure you want to elevate this calendar to a preset?",
+                    icon: "warning",
+                    showCancelButton: true,
+                    cancelButtonColor: '#d33',
+                    confirmButtonColor: '#3085d6',
+                    confirmButtonText: 'Make it so',
+                    dangerMode: true
+                }).then(result => {
+                    if(result.dismiss || !result.value) return;
+                    // TO-DO Axel - Implement Axios and Laravel side of things
+                    console.log(hash)
+                });
+            }
+
+            $(document).ready(function(){
+                $('#btn_elevate').click(elevate_to_preset);
+            });
+        </script>
+
+        <div class='wrap-collapsible content'>
+            <div class='col-12 mb-2'>
+                <div class='row'>
+                    <button type="button" id='btn_elevate' class='btn btn-lg btn-success btn-block'>Elevate to preset</button>
+                </div>
+            </div>
+        </div>
+
+    @endif
+
 @endsection
