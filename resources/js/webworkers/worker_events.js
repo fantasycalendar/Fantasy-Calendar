a9094e75e2c64f0352829b9eb18722b711f2796d
Haxxer <adamoresten@gmail.com>
2020-08-30 15:22:52 +0100

    Fixed season event rounding issues

--- resources/js/webworkers/worker_events.js
+++ resources/js/webworkers/worker_events.js
@@ -354,8 +354,10 @@ var event_evaluator = {
 
 			}else{
 
-				var begin_epoch = this.current_event.lookback ? event_evaluator.start_epoch-this.current_event.lookback : event_evaluator.start_epoch-this.current_event.data.search_distance;
-				var last_epoch = this.current_event.lookahead ? event_evaluator.end_epoch+this.current_event.lookahead : event_evaluator.end_epoch+this.current_event.data.search_distance;
+				let search_distance = this.current_event.data.search_distance ? this.current_event.data.search_distance : 0;
+
+				var begin_epoch = this.current_event.lookback ? event_evaluator.start_epoch-this.current_event.lookback : event_evaluator.start_epoch-search_distance;
+				var last_epoch = this.current_event.lookahead ? event_evaluator.end_epoch+this.current_event.lookahead : event_evaluator.end_epoch+search_distance;
 
 				for(var epoch = begin_epoch; epoch < last_epoch-1; epoch++){
 
