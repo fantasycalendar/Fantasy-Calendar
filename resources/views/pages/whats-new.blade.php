82751e339f9971640ef9e298798e52481b6b7d9e
V13Axel <alex@stechstudio.com>
2020-09-11 00:31:05 -0400

    This is starting to look pretty nice

--- resources/views/pages/whats-new.blade.php
+++ resources/views/pages/whats-new.blade.php
@@ -82,53 +82,111 @@
 
         <div class="fullheight green-border comparison_image" id="section3" style="background-image: linear-gradient(to right, rgba(255, 255, 255, 0.95), rgba(255, 255, 255, 0.95)), url({{ asset('resources/whats-new-2-0.png') }})">
             <h1 style="position: absolute; top: 8%; width: 100%; text-align: center; color: #2d3748;">This is Fantasy Calendar 2.0.</h1>
-            <h2 style="position: absolute; top: 16%; width: 100%; text-align: center; color: #2d3748;">Lovingly crafted to look good on any screen. Even mobile devices.</h2>
+            <h2 style="position: absolute; top: 16%; width: 100%; text-align: center; color: #2d3748;">Lovingly crafted to be enjoyable.</h2>
             <h3 class="next-link" style="font-size: 40px; position: absolute; bottom: 10px; width: 100%; text-align: center; z-index: 21;"><a href="#section4"><i class="fa fa-chevron-circle-down"></i> Looks great! Show me more <i class="fa fa-chevron-circle-down"></i></a></h3>
             <img src="{{ asset('/resources/whats-new-2-0.png') }}" style="max-width: 72%; max-height: 50%; box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05); position: absolute;">
             <div class="overlay"></div>
         </div>
 
         <div class="section comparison_both welcome" id="section4" style="background-color: #edf2f7;">
-            <div class="container my-5 py-5">
+            <div class="container my-5 pt-5">
                 <div class="row">
-
-                    <div class="col-12 col-md-7 text-center text-md-left">
-                        <img src="{{ asset('resources/whats-new-2-0.png') }}" style="box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);">
+                    <div class="col-12 col-md-5 text-center text-md-left d-flex flex-column align-items-start justify-content-center">
+                        <h1>A fresh, modern look</h1>
                     </div>
 
-                    <div class="col-12 col-md-5 mt-5 text-center text-md-right">
-                        <h1>A fresh, modern look</h1>
-                        <h3>Looks aren't everything, but they certainly help.</h3>
+                    <div class="col-12 col-md-7 text-center text-md-right">
+                        <img src="{{ asset('resources/whats-new-2-0.png') }}" style="box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);">
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="section">
-            <div class="container my-5 py-5">
+        <div class="section comparison_both welcome" id="section4" style="background-color: #edf2f7;">
+            <div class="container mb-5 pb-5">
                 <div class="row">
-                    <div class="col-12 col-md-6 text-center text-md-left">
+
+                    <div class="col-12 col-md-7 text-center text-md-left">
+                        <img src="{{ asset('resources/whats-new-event-colors.png') }}" style="box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);">
+                    </div>
+
+                    <div class="col-12 col-md-5 text-center text-md-right d-flex flex-column align-items-end justify-content-center">
+                        <h4>Make your events colorful, so you and your players can tell events apart at a glance.</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section py-5" style="position: relative; overflow: hidden;">
+            <div class="background" style="background-image: url('{{ asset('resources/gray-cubic-bg.png') }}'); position: absolute; top: 0; left: 0; right: 0; bottom: 0; opacity: 0.25; transform: scale(2.2, 2.2)"></div>
+            <div class="container py-5">
+                <div class="row">
+                    <div class="col-12 col-md-6 text-center text-md-left d-flex flex-column align-items-start justify-content-center">
                         <h3>Great on any device</h3>
                         <h4>For a DM on the go, or for players who like to take notes during the session, Fantasy Calendar 2.0 works great on phones and tablets.</h4>
                     </div>
                     <div class="col-12 col-md-6 text-center text-md-right">
                         {{--                    Placeholder --}}
-                        <img src="{{ asset('resources/whats-new-2-0.png') }}" style="box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);">
+                        <img src="{{ asset('resources/any-device.png') }}">
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="section darkmode" style="background-color: #222222">
-            <div class="container my-5 py-5">
+        <div class="section py-5 darkmode" style="background-color: #222222; background-image: url('{{ asset('resources/gray-square-bg.png') }}');">
+            <div class="container py-5">
                 <div class="row">
                     <div class="col-12 col-md-6 text-center text-md-left">
                         {{--                    Placeholder --}}
-                        <img src="{{ asset('resources/whats-new-2-0.png') }}" style="box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);">
+                        <img src="{{ asset('resources/whats-new-dark.png') }}" style="box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);">
                     </div>
-                    <div class="col-12 col-md-6 text-center text-md-right">
+                    <div class="col-12 col-md-6 text-center text-md-right d-flex flex-column align-items-end justify-content-center">
                         <h3 style="color: white;">What lives in the darkness...</h3>
                         <h4 style="color: white;">That's probably a bit dramatic... But comfort for your retinas is here!</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section py-5" style="background-color: #edf2f7;">
+            <div class="container" style="max-width: 1300px;">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h2>And So Much More...</h2>
+                    </div>
+                </div>
+                <div class="row py-5">
+                    <div class="col-12 col-md-3 py-3 py-md-0 px-2 ml-md-0 pl-md-0 pr-md-1">
+                        <div class="inner text-left h-100 px-3" style="background-color: #e2e8f0;">
+                            <h5 class="pt-2"><i class="fa fa-check-circle"></i> Eras</h5>
+                            <p class="small">AD, BC, Dalereckoning, Post-Divergence, "The&nbsp;Reign of The Ashmarai" ... Whatever you call them, you can now record the eras of your world!</p>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-3 py-3 py-md-0 px-2 px-md-1">
+                        <div class="inner text-left h-100 px-3" style="background-color: #e2e8f0;">
+                            <h5 class="pt-2"><i class="fa fa-rocket"></i> Event Categories</h5>
+                            <p class="small">Holidays, natural world events, kingdom celebrations, character birthdays, campaign notes... Organize your categories for the ways you use them.</p>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-3 py-3 py-md-0 px-2 px-md-1">
+                        <div class="inner text-left h-100 px-3" style="background-color: #e2e8f0;">
+                            <h5 class="pt-2"><i class="fa fa-mobile"></i> Collaboration</h5>
+                            <p class="small">Invite another GM to run events in your world, or let your players help you chronicle the events of the story you're telling together.</p>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-3 py-3 py-md-0 px-2 mr-md-0 pr-md-0 pl-md-1">
+                        <div class="inner text-left h-100 px-3" style="background-color: #e2e8f0;">
+                            <h5 class="pt-2"><i class="fa fa-calendar-day"></i> Engine improvements</h5>
+                            <p class="small">Various improvements under the hood:</p>
+                            <ul>
+                                <li class="small">More accurate leap days</li>
+                                <li class="small">Leap months</li>
+                                <li class="small">Intercalary months</li>
+                                <li class="small">Better weather information</li>
+                                <li class="small">Improved handling of seasons</li>
+                                <li class="small">Right-click support for quick management</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
