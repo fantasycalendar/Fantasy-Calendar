aab71641c4cf8fde11e53e3524f75f311c762fc9
V13Axel <alex@stechstudio.com>
2020-09-24 23:12:30 -0400

    moar

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'FantasyCalendar')
+@if (trim($slot) === 'Fantasy Calendar')
     <img src="{{ asset('resources/logo-email.png') }}" class="logo" alt="Fantasy Calendar Logo" style="margin-bottom: 5px;">
 @else
 {{ $slot }}
