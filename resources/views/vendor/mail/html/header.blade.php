136b965e662fd4dfd025f991567c96dfdef6b9b3
V13Axel <alex@stechstudio.com>
2020-09-24 23:06:04 -0400

    make our emails look pretty

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,11 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'FantasyCalendar')
+    <img src="{{ asset('resources/logo-email.png') }}" class="logo" alt="Fantasy Calendar Logo" style="margin-bottom: 5px;">
+@else
+{{ $slot }}
+@endif
+</a>
+</td>
+</tr>
