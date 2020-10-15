bc4d143294f3c460fbb32c7d71523077a298a5d0
Adam Oresten <adamoresten@gmail.com>
2020-10-15 03:26:07 +0100

    Topic/discord webhooks (#117)
    * Initial commit
    
    * Oops
    
    * Oops electric boogaloo
    
    * Stupid stuff goes in, discord message comes out, it's magic
    
    * rename webhooks_out table to webhooks_sent
    
    * update current usages of the term
    
    * split out to a statistics class
    
    * return codes in console commands should be 1 or 0
    
    * Log webhooks
    
    * Added monthly and yearly subscription stats
    
    * Use right variable
    
    * Added daily task to run discord notification
    
    * dry this up a bit
    
    * grrrrrrrrrrrrrrrrr
    
    * subHours(24)
    
    * only run these once
    
    * file permissions are hard
    
    Co-authored-by: V13Axel <alex@stechstudio.com>
    Co-authored-by: Alex Dills <alex@stechstudios.com>

---
+++ app/Services/Statistics.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\User;
+use Laravel\Cashier\Subscription;
+
+class Statistics
+{
+    /**
+     * @var User
+     */
+    private $user;
+    /**
+     * @var Subscription
+     */
+    private $subscription;
+
+    public function __construct(){
+        $this->user = new User();
+        $this->subscription = new Subscription();
+    }
+
+    public function getUsersVerifiedToday() {
+        $total_users = $this->user->where('active',1)->whereNotNull('date_register')->count();
+        $users_today = $this->user->where('active',1)->where('date_register', '>', now()->subHours(24))->count();
+
+        return $this->format($total_users, $users_today);
+    }
+
+    public function getMonthlySubscribersToday() {
+        $monthly_subscriptions = $this->subscription->query()->where('stripe_plan', '=', 'timekeeper_monthly')->active();
+        $total_monthly_subscriptions = $monthly_subscriptions->count();
+        $monthly_subscriptions_today = $monthly_subscriptions->where('updated_at', '>', now()->subHours(24))->count();
+
+        return $this->format($total_monthly_subscriptions, $monthly_subscriptions_today);
+    }
+
+    public function getYearlySubscribersToday() {
+        $yearly_subscriptions = $this->subscription->query()->where('stripe_plan', '=', 'timekeeper_yearly')->active();
+        $total_yearly_subscriptions = $yearly_subscriptions->count();
+        $yearly_subscriptions_today = $yearly_subscriptions->where('updated_at', '>', now()->subHours(24))->count();
+
+        return $this->format($total_yearly_subscriptions, $yearly_subscriptions_today);
+    }
+
+    public function format($total, $today) {
+        return sprintf("%s (%s)", $total, (($today) ? $today: "No change"));
+    }
+}
