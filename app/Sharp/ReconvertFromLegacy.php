02d55e2d37a4809b15b2d63083a488ae3d5cc159
V13Axel <alex@stechstudio.com>
2020-10-17 13:24:29 -0400

    actually wire up the reconvert job

--- app/Sharp/ReconvertFromLegacy.php
+++ app/Sharp/ReconvertFromLegacy.php
@@ -3,6 +3,7 @@
 namespace App\Sharp;
 
 use App\Calendar;
+use App\Jobs\ConvertCalendarTo2Point0;
 use App\OldCalendar;
 use Code16\Sharp\EntityList\Commands\InstanceCommand;
 
@@ -28,6 +29,8 @@ class ReconvertFromLegacy extends InstanceCommand
      */
     public function execute($instanceId, array $data = []): array
     {
-        //
+        ConvertCalendarTo2Point0::dispatchNow(OldCalendar::where('hash', Calendar::find($instanceId)->hash)->firstOrFail(), Calendar::max('conversion_batch') + 1 ?? 1);
+
+        return $this->reload();
     }
 }
