64732ddcb36a34b89539caf9abe42ec778d5d9f4
Haxxer <adamoresten@gmail.com>
2020-10-18 22:50:33 +0100

    Fix this lil problemo

--- resources/views/templates/footnote.blade.php
+++ resources/views/templates/footnote.blade.php
@@ -1,3 +1,3 @@
 <section class="footer container text-center p-3 mb-3" style="max-width: 800px; opacity: 0.5;">
-    <span class="copyright">Copyright © {{ date('Y') }} Fantasy Computerworks Ltd - <a href="{{ route('terms-of-service') }}">Terms of Service</a> - <a href="{{ route('privacy-policy') }}">Privacy Policy</a></div>
+    <span class="copyright">Copyright © {{ date('Y') }} Fantasy Computerworks Ltd - <a href="{{ route('terms-of-service') }}">Terms of Service</a> - <a href="{{ route('privacy-policy') }}">Privacy Policy</a></span>
 </section>
