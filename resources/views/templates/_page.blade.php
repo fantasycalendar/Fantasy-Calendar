a02688a608d34fb73c8af7f0c5ad217cb075af56
V13Axel <alex@stechstudio.com>
2020-10-27 23:05:06 -0400

    update discord embeds with new logo

--- resources/views/templates/_page.blade.php
+++ resources/views/templates/_page.blade.php
@@ -12,6 +12,7 @@
     <meta property="og:type" content="website">
     <meta property="og:url" content="{{ url()->full() }}">
     <meta property="og:image" content="{{ asset('resources/discord_logo.jpg') }}">
+    <meta property="og:description" content="All-in-One Fantasy Calendar Generator - Creation of calendars and time-tracking in your homebrew or pre-made campaign worlds have never been easier!">
 
     @if(Auth::check())
         <meta name='api-token' content="{{ Auth::user()->api_token }}">
