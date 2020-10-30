349f9103f0bc3c04e7920c1680fe684994f5385c
V13Axel <alex@stechstudio.com>
2020-10-29 23:48:48 -0400

    this is no longer true

--- resources/views/pages/account-migrated.blade.php
+++ resources/views/pages/account-migrated.blade.php
@@ -24,7 +24,7 @@
     <div class="container py-5 d-md-flex justify-content-md-center align-items-md-center h-100 w-100">
         <div class="welcome text-md-center p-5" style="background-color: ">
             <h2>Welcome to Fantasy Calendar 2.0!</h2>
-            <p class="mb-5" style="opacity: 0.8;">Don't worry, your calendars are all still just where you left them, but they've been made <span>better!</span> <br>2.0 is entirely new though, which means you'll need to login again. <br>Sorry about that.</p>
+            <p class="mb-5" style="opacity: 0.8;">Don't worry, your calendars are all still just where you left them, but they've been made <span>better!</span></p>
             <a href="{{ route('account-migrated-acknowledge') }}" class="mb-3 btn btn-lg btn-accent text-white">Take a look to see them now!</a><br>
             <a href="{{ route('whats-new') }}" class="mb-3 btn btn-outline-secondary">What's new in 2.0?</a>
         </div>
