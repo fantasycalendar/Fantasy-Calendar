ac93d126dbb5973b376a52092e10c874150a88b3
V13Axel <alex@stechstudio.com>
2020-10-29 23:12:39 -0400

    here too

--- resources/views/templates/_page.blade.php
+++ resources/views/templates/_page.blade.php
@@ -54,6 +54,9 @@
                     'Authorization': 'Bearer '+$('meta[name="api-token"]').attr('content')
                 }
             });
+            
+            var cookiedomain = window.location.hostname.split('.')[window.location.hostname.split('.').length-2]+'.'+window.location.hostname.split('.')[window.location.hostname.split('.').length-1];
+            document.cookie = 'fantasycalendar_remember=; Max-Age=0; path=/; domain=' + cookiedomain;
         });
     </script>
 
