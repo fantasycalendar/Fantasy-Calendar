388a120705dcb409c8090e26fd60c0be47b0287e
V13Axel <alex@stechstudio.com>
2020-11-06 00:01:27 -0500

    swanky footer

--- resources/views/templates/footnote.blade.php
+++ resources/views/templates/footnote.blade.php
@@ -13,10 +13,27 @@
     section.footer a:hover {
         color: rgba(255, 255, 255, 0.6);
     }
+    section.footer .footer-social-icons a i {
+        background-color: rgb(47, 133, 90);
+        padding: 5px;
+        color: #323232;
+        font-size: 24px;
+    }
+    section.footer .footer-social-icons a:hover i {
+        background-color: rgba(255, 255, 255, 0.3);
+    }
 </style>
 
 <section class="footer d-flex flex-column flex-md-row mt-5 mt-md-0">
-    <div class="logo pb-4 pb-md-0"><img src="{{ asset('resources/footer_logo.png') }}"></div>
+    <div class="logo pb-4 pb-md-0 d-flex flex-column flex-md-row align-items-center">
+        <img src="{{ asset('resources/footer_logo.png') }}">
+        <div class="footer-social-icons px-md-2 border-md-left border-secondary ml-md-2 mt-2 pt-2 pt-md-0 mt-md-0">
+            <a href="https://discord.com/invite/9jjQuzax"><i class="fab fa-discord mx-1 rounded"></i></a>
+            <a href="https://twitter.com/FantasyCalendar"><i class="fab fa-twitter mx-1 rounded"></i></a>
+            <a href="https://github.com/fantasycalendar"><i class="fab fa-github mx-1 rounded"></i></a>
+        </div>
+    </div>
+
     <div class="copyright text-center text-md-right">
         <span class="copyright d-inline-block mb-2">Copyright © {{ date('Y') }} Fantasy Computerworks Ltd</span><br>
         <span><a href="{{ route('terms-and-conditions') }}">Terms and Conditions</a> - <a href="{{ route('privacy-policy') }}">Privacy and Cookies Policy</a></span>
