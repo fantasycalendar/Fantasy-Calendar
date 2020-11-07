b9ac0b54c412a940800fd69a937e62217c4d28d0
Haxxer <adamoresten@gmail.com>
2020-11-07 15:27:52 +0000

    Unescape ALL the characters

--- resources/views/calendar/_loadcalendar.blade.php
+++ resources/views/calendar/_loadcalendar.blade.php
@@ -19,7 +19,7 @@
 
 hash = `{{ $calendar->hash }}`;
 
-calendar_name = `{!! $calendar->name !!}`;
+calendar_name = unescapeHtml("{{ $calendar->name }}");
 calendar_id = {{ $calendar->id }};
 static_data = {!! json_encode($calendar->static_data) !!};
 dynamic_data = {!! json_encode($calendar->dynamic_data) !!};
