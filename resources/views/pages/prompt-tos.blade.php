897a71a189fa7ab38d90c9d452c32fb6e600bd68
Haxxer <adamoresten@gmail.com>
2020-11-07 17:33:05 +0000

    Fixed prompt tos sometimes hiding accept button under footer

--- resources/views/pages/prompt-tos.blade.php
+++ resources/views/pages/prompt-tos.blade.php
@@ -34,43 +34,49 @@
 
 @section("content")
 
-
-
 	<div class="container p-4" x-data="agreement_form()">
+		
+		<div class='row'>
+			<div class="alert alert-info" x-show="!dismissed"><a href="#" class="alert-link" style="float: right;" @click="dismissed = true"><i class="fa fa-times"></i></a> Sorry - a lot has changed in the <a class="alert-link" href="{{ route('whats-new') }}">2.0 update</a>, and since those changes involve collaboration and optional paid services, we had to get our lawyers involved. You'll need to accept the Terms and Conditions, and the Privacy and Cookies Policy before you can continue using the site.</div>
+		</div>
 
-		<div class="alert alert-info" x-show="!dismissed"><a href="#" class="alert-link" style="float: right;" @click="dismissed = true"><i class="fa fa-times"></i></a> Sorry - a lot has changed in the <a class="alert-link" href="{{ route('whats-new') }}">2.0 update</a>, and since those changes involve collaboration and optional paid services, we had to get our lawyers involved. You'll need to accept the Terms and Conditions, and the Privacy and Cookies Policy before you can continue using the site.</div>
+		<div class='row'>
+			<div class='col'>
 
-		<h1>{{ $title }}</h1>
-		<p><i>Document Version {{ $version }}.0 — {{ $date }}</i></p>
-		<div class="scroll_box mb-2 border p-3 rounded">{!! Markdown::convertToHtml($markdown); !!}</div>
+				<h1>{{ $title }}</h1>
+				<p><i>Document Version {{ $version }}.0 — {{ $date }}</i></p>
+				<div class="scroll_box mb-2 border p-3 rounded">{!! Markdown::convertToHtml($markdown); !!}</div>
 
-		<form method="GET" action="{{ route('agreement-accepted', ['intended' => $intended]) }}">
+				<form method="GET" action="{{ route('agreement-accepted', ['intended' => $intended]) }}">
 
-			<div class="form-check border rounded p-2 mb-2">
-				<input type="checkbox" class="form-check-input" id="policy_acceptance" name="policy_acceptance" x-model="agreed" required>
+					<div class="form-check border rounded p-2 mb-2">
+						<input type="checkbox" class="form-check-input" id="policy_acceptance" name="policy_acceptance" x-model="agreed" required>
 
-				<label class="form-check-label" for="policy_acceptance">By clicking <strong>Agree and Continue</strong>, I agree to the <a target="_blank" href="{{ route('terms-and-conditions') }}">Terms and Conditions</a>, and the <a target="_blank" href="{{ route('privacy-policy') }}">Privacy and Cookies Policy</a></label>
+						<label class="form-check-label" for="policy_acceptance">By clicking <strong>Agree and Continue</strong>, I agree to the <a target="_blank" href="{{ route('terms-and-conditions') }}">Terms and Conditions</a>, and the <a target="_blank" href="{{ route('privacy-policy') }}">Privacy and Cookies Policy</a></label>
+					</div>
+
+					<div class="form-check border rounded p-2 mb-2">
+						<input type="checkbox" class="form-check-input" id="marketing_acceptance" name="marketing_acceptance">
+						<label class="form-check-label" for="marketing_acceptance">
+							<strong>Optional</strong> - Tick here if you would like us to send you emails about our products and special offers<br>
+							<small>Don't worry - We won't spam you. We'll only send emails for major updates or new products we develop, and you can withdraw consent anytime on your profile.<small>
+						</label>
+					</div>
+
+					<button
+						type="submit"
+						class="btn btn-lg"
+						:disabled="!agreed",
+						:class="{
+							'btn-secondary': !agreed,
+							'btn-primary': agreed
+						}"
+					>Agree and Continue</button>
+
+				</form>
+
 			</div>
-
-			<div class="form-check border rounded p-2 mb-2">
-				<input type="checkbox" class="form-check-input" id="marketing_acceptance" name="marketing_acceptance">
-				<label class="form-check-label" for="marketing_acceptance">
-					<strong>Optional</strong> - Tick here if you would like us to send you emails about our products and special offers<br>
-					<small>Don't worry - We won't spam you. We'll only send emails for major updates or new products we develop, and you can withdraw consent anytime on your profile.<small>
-				</label>
-			</div>
-
-			<button
-				type="submit"
-				class="btn btn-lg"
-				:disabled="!agreed",
-				:class="{
-					'btn-secondary': !agreed,
-					'btn-primary': agreed
-				}"
-			>Agree and Continue</button>
-
-		</form>
+		</div>
 
 	</div>
 
