0c16424ba80b5bfb6f8224a8758367c66b3c17bb
V13Axel <alex@stechstudio.com>
2020-11-09 13:42:44 -0500

    let's make this more foolproof - Heaven knows I need it.

--- app/Console/Commands/SendAnnouncementEmail.php
+++ app/Console/Commands/SendAnnouncementEmail.php
@@ -40,19 +40,29 @@ class SendAnnouncementEmail extends Command
      */
     public function handle()
     {
-        $users = User::whereNull('agreed_at');
+        $users = User::whereNull('deleted_at')->where('has_sent_announcement', 0);
+
+        if(!$this->option('where') && !$this->option('ids')) {
+            $this->error("You must supply either a where clause (--where=\"date_register > '2020-01-01'\") or a list of IDs (--ids=1,2,3,500,10000,10)");
+            exit(1);
+        }
 
         if($where = $this->option('where')) {
-            $users = $users->whereRaw($where);
+            $users = $users->whereNull('agreed_at')->whereRaw($where);
         } else if($ids = $this->option('ids')) {
             $users = $users->whereIn('id', explode(',', $ids));
         }
 
-        if($users->count() > 500 && !$this->confirm(sprintf('Found %d users, convert them all?', $users->count()))) {
-            $this->warn('Stopping here, no users were converted.');
+        if(!$users->count()) {
+            $this->error("No users matched your search. Check your SQL or your IDs and try again.");
             exit(1);
         }
-        
+
+        if($users->count() > 500 && !$this->confirm(sprintf('Found %d users, send them all?', $users->count()))) {
+            $this->warn('Stopping here, no users were sent to.');
+            exit(1);
+        }
+
         $users->chunk(10, function($users){
             $users->each(function($user){
                 if(!$user->has_sent_announcement || $this->option('force')){
