26fe9da4a1221774d92385e938a4fbc1b0242530
V13Axel <alex@stechstudio.com>
2021-03-14 19:17:49 -0400

    basic structure of connect page

--- app/Services/Discord/resources/views/pages/connect-account.blade.php
+++ app/Services/Discord/resources/views/pages/connect-account.blade.php
@@ -1,5 +1,21 @@
 @extends('templates._page')
 
 @section('content')
-    This is a page
+    <div class="container py-5">
+        <h3>Connect your Fantasy Calendar account with Discord!</h3>
+        <h5 style="opacity: 0.65;">Don't worry, we only track the minimum necessary to make things work. As Discord will tell you, neither of the options below lets us read your messages or anything like that.</h5>
+
+        <div class="row">
+            <div class="col-12">
+                <h4>I want to...</h4>
+            </div>
+            <div class="col-12 col-md-6 border rounded">
+                <h5>Use Fantasy Calendar in someone else's Discord server</h5>
+            </div>
+            <div class="col-12 col-md-6 border rounded">
+                <h5>Use Fantasy Calendar in my own Discord server</h5>
+            </div>
+        </div>
+
+    </div>
 @endsection
