1f75d560236c0dc3225b7049721317d8bd160ef8
V13Axel <alex@stechstudio.com>
2021-04-02 14:34:43 -0400

    multibyte string replacement

--- app/helpers.php
+++ app/helpers.php
@@ -5,3 +5,17 @@ if(!function_exists('helplink')) {
         return "https://helpdocs.fantasy-calendar.com/" . (($target) ? "topic/" . $target : "");
     }
 }
+
+if(!function_exists('mb_substr_replace')) {
+    function mb_substr_replace($str, $repl, $start, $length = null)
+    {
+        $length = is_int($length) ? $length : utf8_strlen($str);
+        $repl = str_repeat($repl, $length);
+
+        preg_match_all('/./us', $str, $ar);
+        preg_match_all('/./us', $repl, $rar);
+
+        array_splice($ar[0], $start, $length, $rar[0]);
+        return implode($ar[0]);
+    }
+}
