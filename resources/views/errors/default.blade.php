4afc50b459698874d9951ab493d704fd58cf9965
V13Axel <alex@stechstudio.com>
2021-04-30 00:05:00 -0400

    error page fixes

--- resources/views/errors/default.blade.php
+++ resources/views/errors/default.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container py-5 px-3 error-container">
-        <div class="row">
+        <div class="row w-100">
             <div class="d-none d-lg-flex col-lg-5 flex-column justify-content-center">
                 <div class="sketch">
                     <div class="bee-sketch red"></div>
