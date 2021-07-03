fbd0201a04d7ea3b58169195814086fd819f148d
V13Axel <alex@stechstudio.com>
2021-07-03 16:05:45 -0400

    Remove unnecessary use

--- app/Collections/EpochsCollection.php
+++ app/Collections/EpochsCollection.php
@@ -5,7 +5,6 @@ namespace App\Collections;
 
 
 use App\Services\EpochService\Epoch;
-use Illuminate\Support\Collection;
 use Illuminate\Support\Collection as BaseCollection;
 
 class EpochsCollection extends BaseCollection
