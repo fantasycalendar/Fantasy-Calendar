97c988a876d24c5ecb9c92a42cb7b64abe667c56
Haxxer <adamoresten@gmail.com>
2021-08-31 23:23:16 +0100

    Make more better

--- resources/views/emails/discord-announcement.blade.php
+++ resources/views/emails/discord-announcement.blade.php
@@ -3,23 +3,23 @@
 
 Dear {{ $user->username }},
 
-We are proud to announce that we have released our very own **Discord integration**! You can access your calendars, set their dates, and show it off to your players — all from the comfort of your own Discord server.
+We are proud to announce our very own **Discord integration**! Access your calendars, set their dates, and show it off to your players — all from the comfort of your own Discord server.
 
 ![Image showing adding days and displaying a calendar in Discord]({{ asset('resources/discord/discord_show_month.png') }})
 
 @if($user->isPremium())
-It's quick to setup, easy to use, and available for you **right now**!
+It's quick to setup, easy to use, and available **right now**!
 
 As you're a subscriber, we'd like to take this opportunity to thank you for your support. Fantasy-Calendar is a passion project, and we could not have done it without you!
 @else
-Quick to setup, easy to use, and available **right now** to subscribers. Only $2.49/month (or $24.99/year)!
+Quick to setup, easy to use, and available **right now** for subscribers. Only $2.49/month (or $24.99/year)!
 @endif
 
 @component('mail::button', ['url' => URL::route('discord')])
     Check it out!
 @endcomponent
 
-This release also has a lot of bugfixes and updates, so be sure to take head on over to the [changelog]({{URL::route('changelog')}})!
+This release also contains a lot of bugfixes and updates, so be sure to read over the [changelog]({{URL::route('changelog')}})!
 
 Thanks,<br>
 The {{ config('app.name') }} team
