a3abb7186c38a600470cd05a89a52e63770a07dd
Haxxer <adamoresten@gmail.com>
2021-09-04 11:40:29 +0100

    Show time on /fc show date

--- app/Services/Discord/Commands/Command/Show/MonthHandler.php
+++ app/Services/Discord/Commands/Command/Show/MonthHandler.php
@@ -4,6 +4,7 @@
 namespace App\Services\Discord\Commands\Command\Show;
 
 
+use App\Calendar;
 use App\Services\Discord\Commands\Command\Traits\PremiumCommand;
 use App\Services\Discord\Exceptions\DiscordCalendarNotSetException;
 use App\Services\Discord\Commands\Command;
@@ -13,6 +14,8 @@ class MonthHandler extends Command
 {
     use PremiumCommand;
 
+    private Calendar $calendar;
+
     /**
      * Generate a
      *
@@ -21,6 +24,12 @@ class MonthHandler extends Command
      */
     public function handle(): string
     {
-        return $this->codeBlock(TextRenderer::renderMonth($this->getDefaultCalendar()));
+        $this->calendar = $this->getDefaultCalendar();
+
+        $current_time = ($this->calendar->clock_enabled && !$this->calendar->setting('hide_clock'))
+            ? "Current time: ". $this->calendar->current_time
+            : "";
+
+        return $current_time . $this->codeBlock(TextRenderer::renderMonth($this->calendar));
     }
 }
