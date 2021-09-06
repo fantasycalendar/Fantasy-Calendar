503608cfa6042c670b389620d37e6ddda1475b2c
V13Axel <alex@stechstudio.com>
2021-09-06 13:21:17 -0400

    Clean up and document CommandDispatcher

--- app/Services/Discord/Commands/CommandDispatcher.php
+++ app/Services/Discord/Commands/CommandDispatcher.php
@@ -20,6 +20,13 @@ class CommandDispatcher
         3 => 'Component'
     ];
 
+    /**
+     * Accepts the entirety of a Discord interaction, routes it to our handlers accordingly, based on type
+     * (https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object)
+     *
+     * @param $discordInteraction
+     * @return array
+     */
     public static function dispatch($discordInteraction): array
     {
         return self::failGracefully(function() use ($discordInteraction) {
@@ -27,6 +34,13 @@ class CommandDispatcher
         })->getMessage();
     }
 
+    /**
+     * Dispatches user commands run directly via calling a slash command in Discord to the appropriate handler.
+     * To clarify, that means *only* slash commands, not button clicks or similar.
+     *
+     * @param $commandData
+     * @return Response
+     */
     public static function dispatchCommand($commandData): Response
     {
         $handlerClass = config('services.discord.command_handlers' . self::processConfigPath($commandData['data']));
@@ -35,6 +49,15 @@ class CommandDispatcher
             ->do_handle();
     }
 
+    /**
+     * Dispatches component interactions, such as someone clicking a button or other "component" from us.
+     * This assumes the component was created with a "custom_id" or "target" matching the Laravel config
+     * path for the handler we want. For example, if you want 'services.discord.command_handlers.fc.help'
+     * then your target should be 'fc.help' - And our component classes add the `fc` part of that for us.
+     *
+     * @param $interactionData
+     * @return Response
+     */
     public static function dispatchComponent($interactionData): Response
     {
         $interactionIdParts = explode(':', $interactionData['data']['custom_id']);
@@ -47,11 +70,29 @@ class CommandDispatcher
             ->$handlerFunction(...$args);
     }
 
-    public static function dispatchPong($discordInteraction)
+    /**
+     * Discord may occasionally reach out to us with a 'ping' just to make sure we're checking
+     * for the appropriate signature on their requests to us. This just returns a 'pong' to
+     * make them happy. We've outsourced the format of that pong to the Response class.
+     *
+     * @param $discordInteraction
+     * @return Response
+     */
+    public static function dispatchPong($discordInteraction): Response
     {
         return Response::pong();
     }
 
+    /**
+     * Processes Discord's 'options within options, within more options' approach of providing arguments,
+     * recursively resolving the options by name into handlers within our services config. This approach
+     * means we can either have one big handler for 'fc.add' and its sub-commands OR individual handlers
+     * for sub-commands, such as 'fc.add.day` and `fc.add.month`. This approach may change in the future.
+     *
+     * @param $optionsData
+     * @param string $soFar
+     * @return false|string
+     */
     public static function processConfigPath($optionsData, $soFar = '.')
     {
         $option = Arr::get($optionsData, '0', $optionsData);
@@ -72,18 +113,21 @@ class CommandDispatcher
         return substr($soFar, 0, strlen($soFar) - 1);
     }
 
-    public static function failGracefully(callable $function)
+    /**
+     * This is just some syntactic sugar to try/catch and handle our custom DiscordExceptions.
+     *
+     * @param callable $function
+     * @return Response
+     */
+    public static function failGracefully(callable $function): Response
     {
         try {
             return $function();
         } catch (DiscordException $e) {
             return $e->getResponse();
         } catch (\Throwable $e) {
-            Log::error($e->getMessage());
-            Log::error($e->getTraceAsString());
-
             $errorToReturn = app()->environment('production')
-                ? "Oops! There was an error. This was probably our fault, not yours."
+                ? "Oops! There was an error. This was probably our fault, not yours. We've been notified about it, and will try to get it fixed when we can."
                 : $e->getMessage();
 
             return Response::make($errorToReturn)->ephemeral();
