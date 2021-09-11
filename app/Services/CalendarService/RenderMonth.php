c974dde3653753f4f874390d44fcae27aab68678
V13Axel <alex@stechstudio.com>
2021-09-11 18:43:03 -0400

    Fix shortest day name checker

--- app/Services/CalendarService/RenderMonth.php
+++ app/Services/CalendarService/RenderMonth.php
@@ -65,7 +65,7 @@ class RenderMonth
      * @param null $length
      * @return int|mixed
      */
-    private function findShortestUniquePrefixLength($weekdays, $length = null, $starting = null): int
+    private function findShortestUniquePrefixLength($weekdays, $length = null): int
     {
         $length = $length ?? $weekdays->map(function($weekday) {
                 return strlen($weekday);
@@ -75,15 +75,8 @@ class RenderMonth
             return Str::limit($dayName, $length, '');
         })->max();
 
-        if(!$starting && $matchedShortNames > 1) {
-            $starting = $matchedShortNames;
-            $matchedShortNames = 1;
-        } else if($starting == $matchedShortNames) {
-            $matchedShortNames = 1;
-        }
-
         return ($matchedShortNames === 1)
-            ? $this->findShortestUniquePrefixLength($weekdays, $length - 1, $starting) // All unique, check one more level
-            : $length + 1; // Found duplicates! That means our length is truncating too far.
+            ? $this->findShortestUniquePrefixLength($weekdays, $length - 1) // All unique, check one more level
+            : max($length + 1, 3); // Found duplicates! That means our length is truncating too far.
     }
 }
