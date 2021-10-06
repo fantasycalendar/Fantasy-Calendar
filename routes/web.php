d3b468f2d6c504836dacf2c04ab841b96b962cc6
V13Axel <alex@stechstudio.com>
2021-10-05 20:31:44 -0400

    Remove test route

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,6 @@ Route::get('/imagetest', function(){
     return view('pages.imagetest');
 });
 
-Route::get('/mailable', function () {
-    $user = App\User::find(1);
-    return new \App\Mail\DiscordAnnouncement($user);
-});
-
 Route::get('/', 'WelcomeController@welcome')->name('home');
 Route::view('/welcome', 'welcome')->name('welcome');
 Route::view('/whats-new', 'pages.whats-new')->name('whats-new');
