0cb481c05d024f776efa9bd140b153bed83b5dc9
Haxxer <adamoresten@gmail.com>
2021-10-12 21:12:20 +0100

    Put commonly used methods into the base testcase class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,23 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function getEdgeCases()
+    {
+        return collect(Storage::disk('base')->files('setup/extra-preset-jsons/edge-case-calendars'))
+            ->map(function($file){
+                return Str::replace('.json', '', $file);
+            })->toArray();
+    }
+
+    protected function retrieveJson($presetFile)
+    {
+        return json_decode(file_get_contents(base_path($presetFile). '.json'), true);
+    }
 }
