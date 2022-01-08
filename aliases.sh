f5f634bb4a5d8fc97c96683a8ed0136d42e077a3
Haxxer <adamoresten@gmail.com>
2022-01-08 15:35:41 +0000

    Revert accidental push

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,4 @@
-alias npm='docker run -it -u $(id -u):$(id -g) -v ${PWD}/:/app -w /app node:14 npm'
+alias npm='docker run -it -u $(id -u):$(id -g) -v ${PWD}/:/app -w /app node npm'
 alias artisan='docker-compose exec php php artisan'
 alias fc_up='FC_WEB_PORT=9987 FC_BROWSERSYNC_PORT=9980 FC_BROWSERSYNC=true docker-compose up'
 alias fc_nohot='docker-compose up'
