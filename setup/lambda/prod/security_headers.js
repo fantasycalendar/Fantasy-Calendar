7ef1f02ffb0d4e7c6680a53a356b7526f8018859
V13Axel <alex@stechstudio.com>
2022-01-30 10:45:03 -0500

    Remove x-frame-options header

--- setup/lambda/prod/security_headers.js
+++ setup/lambda/prod/security_headers.js
@@ -8,9 +8,9 @@ exports.handler = (event, context, callback) => {
     //Set new headers
     headers['strict-transport-security'] = [{key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubdomains; preload'}];
     headers['x-content-type-options'] = [{key: 'X-Content-Type-Options', value: 'nosniff'}];
-    headers['x-frame-options'] = [{key: 'X-Frame-Options', value: 'SAMEORIGIN'}]
     headers['x-xss-protection'] = [{key: 'X-XSS-Protection', value: '1; mode=block'}];
     headers['referrer-policy'] = [{key: 'Referrer-Policy', value: 'same-origin'}];
+    delete headers['x-frame-options'];
 
     //Return modified response
     callback(null, response);
