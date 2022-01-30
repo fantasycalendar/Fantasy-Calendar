3e8255c3fdd55f97dcea626005b3941ce7c000a3
V13Axel <alex@stechstudio.com>
2022-01-29 22:21:51 -0500

    SAMEORIGIN

--- setup/lambda/prod/security_headers.js
+++ setup/lambda/prod/security_headers.js
@@ -1,17 +1,17 @@
 'use strict';
 
-exports.handler = (event, context, callback) => {    
+exports.handler = (event, context, callback) => {
     //Get contents of response
     const response = event.Records[0].cf.response;
     const headers = response.headers;
 
-    //Set new headers 
-    headers['strict-transport-security'] = [{key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubdomains; preload'}]; 
-    headers['x-content-type-options'] = [{key: 'X-Content-Type-Options', value: 'nosniff'}]; 
-    headers['x-frame-options'] = [{key: 'X-Frame-Options', value: 'DENY'}]; 
-    headers['x-xss-protection'] = [{key: 'X-XSS-Protection', value: '1; mode=block'}]; 
-    headers['referrer-policy'] = [{key: 'Referrer-Policy', value: 'same-origin'}]; 
-    
+    //Set new headers
+    headers['strict-transport-security'] = [{key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubdomains; preload'}];
+    headers['x-content-type-options'] = [{key: 'X-Content-Type-Options', value: 'nosniff'}];
+    headers['x-frame-options'] = [{key: 'X-Frame-Options', value: 'SAMEORIGIN'}]
+    headers['x-xss-protection'] = [{key: 'X-XSS-Protection', value: '1; mode=block'}];
+    headers['referrer-policy'] = [{key: 'Referrer-Policy', value: 'same-origin'}];
+
     //Return modified response
     callback(null, response);
 };
