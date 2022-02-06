454a5a12a98a176ee604486138af6e5924dc0373
V13Axel <alex@stechstudio.com>
2022-02-06 18:53:25 -0500

    Avatar

--- resources/views/profile/account.blade.php
+++ resources/views/profile/account.blade.php
@@ -5,9 +5,14 @@
 
             <div class="shadow sm:rounded-md sm:overflow-hidden">
                 <div class="bg-white dark:bg-gray-800 py-6 px-4 sm:p-6">
-                    <div>
-                        <h2 id="user-details-heading" class="text-lg leading-6 font-medium text-gray-900 dark:text-gray-200">{{ auth()->user()->username }}</h2>
-                        <p class="mt-1 text-sm text-gray-500 dark:text-gray-400"><i class="fa fa-user-circle"></i> Registered {{ auth()->user()->created_at->format('Y-m-d') }} <i class="fa fa-ellipsis-h px-2"></i> <i class="fa fa-calendar"></i> {{ auth()->user()->calendars()->count() }} {{ \Illuminate\Support\Str::plural('Calendar', auth()->user()->calendars()->count()) }}</p>
+                    <div class="flex items-center">
+                        <div class="h-12 w-12 rounded-full overflow-hidden mr-3 bg-gray-900 dark:bg-gray-500 border-2 border-gray-900 dark:border-gray-500">
+                            <img src="https://unavatar.now.sh/{{ auth()->user()->email }}?fallback=http://beta.fantasy-calendar.com/resources/logo-dark.png" alt="" class="h-full w-full">
+                        </div>
+                        <div>
+                            <h2 id="user-details-heading" class="text-lg leading-6 font-medium text-gray-900 dark:text-gray-200">{{ auth()->user()->username }}</h2>
+                            <p class="mt-1 text-sm text-gray-500 dark:text-gray-400"><i class="fa fa-user-circle"></i> Registered {{ auth()->user()->created_at->format('Y-m-d') }} <i class="fa fa-ellipsis-h px-2"></i> <i class="fa fa-calendar"></i> {{ auth()->user()->calendars()->count() }} {{ \Illuminate\Support\Str::plural('Calendar', auth()->user()->calendars()->count()) }}</p>
+                        </div>
                     </div>
 
                     <div class="mt-6 grid grid-cols-4 gap-6">
--- resources/views/profile/integrations.blade.php
+++ resources/views/profile/integrations.blade.php
@@ -15,36 +15,6 @@
                             <x-text-input value="{{ old('email') ?? auth()->user()->email }}" label="Email Address" type="text" name="email" id="email" autocomplete="email"></x-text-input>
                         </div>
 
-                        <div class="col-span-4 sm:col-span-2">
-                            <x-text-input label="Confirm Email" value="{{ old('email_confirmation') ?? auth()->user()->email }}" type="text" name="email_confirmation" id="email_confirmation" autocomplete="email"></x-text-input>
-                        </div>
-
-                        @if(session('alerts') && array_key_exists('email-success', session('alerts')))
-                            <div class="col-span-4">
-                                <x-alert type="success">{{ session('alerts')['email-success'] }}</x-alert>
-                            </div>
-                        @endif
-
-                        @if(session('alerts') && array_key_exists('email', session('alerts')))
-                            <div class="col-span-4">
-                                <x-alert type="notice">{{ session('alerts')['email'] }}</x-alert>
-                            </div>
-                        @endif
-
-                        <div class="col-span-4 sm:col-span-2">
-                            <x-text-input label="New Password" type="password" name="password" id="password" placeholder="************"></x-text-input>
-                        </div>
-
-                        <div class="col-span-4 sm:col-span-2">
-                            <x-text-input label="Confirm New Password" type="password" name="password_confirmation" id="password_confirmation" placeholder="************"></x-text-input>
-                        </div>
-
-                        @if(session('alerts') && array_key_exists('password', session('alerts')))
-                            <div class="col-span-4">
-                                <x-alert type="success">{{ session('alerts')['password'] }}</x-alert>
-                            </div>
-                        @endif
-
                     </div>
                 </div>
                 <div class="px-4 py-3 bg-gray-50 dark:bg-gray-800 dark:border-t dark:border-gray-700 text-right sm:px-6">
