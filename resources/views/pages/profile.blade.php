695cbec21669357884860a476cdd3111b69db803
V13Axel <alex@stechstudio.com>
2022-02-06 14:53:59 -0500

    Even cleaner

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -1,50 +1,15 @@
 <x-app-layout>
-    <!--
-      This example requires Tailwind CSS v2.0+
-
-      This example requires some changes to your config:
-
-      ```
-      // tailwind.config.js
-      const colors = require('tailwindcss/colors')
-
-      module.exports = {
-        // ...
-        theme: {
-          extend: {
-            colors: {
-              primary: colors.primary,
-            },
-          },
-        },
-        plugins: [
-          // ...
-          require('@tailwindcss/forms'),
-        ],
-      }
-      ```
-    -->
-    <!--
-      This example requires updating your template:
-
-      ```
-      <html class="h-full bg-gray-100">
-      <body class="h-full">
-      ```
-    -->
     <div class="h-full">
         <main class="max-w-7xl mx-auto pb-10 lg:px-8">
             <div class="lg:grid lg:grid-cols-12 lg:gap-x-5">
                 <aside class="py-6 px-2 sm:px-6 lg:py-0 lg:px-0 lg:col-span-3">
                     <nav class="space-y-1">
-                        <!-- Current: "bg-gray-50 text-primary-600 hover:bg-white", Default: "text-gray-900 hover:text-gray-900 hover:bg-gray-50" -->
                         <x-left-nav-item icon="cog" label="Account" route="profile"></x-left-nav-item>
                         <x-left-nav-item icon="credit-card" label="Plan & Billing" route="profile.billing"></x-left-nav-item>
                         <x-left-nav-item icon="puzzle-piece" label="Integrations" route="profile.billing"></x-left-nav-item>
                     </nav>
                 </aside>
 
-                <!-- Payment details -->
                 <div class="space-y-6 sm:px-6 lg:px-0 lg:col-span-9">
                     <section aria-labelledby="user-details-heading">
                         <form action="#" method="POST">
@@ -73,7 +38,7 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="px-4 py-3 bg-gray-50 dark:bg-gray-700 text-right sm:px-6">
+                                <div class="px-4 py-3 bg-gray-50 dark:bg-gray-800 dark:border-t dark:border-gray-700 text-right sm:px-6">
                                     <x-button type="submit">Save</x-button>
                                 </div>
                             </div>
@@ -95,7 +60,7 @@
                                         <x-input-toggle name="marketing_acceptance" label="Send me product updates" description="We'll only send you stuff when it's a super big deal - No spam here." value="{{ $user->marketing ? 'true' : 'false' }}"></x-input-toggle>
                                     </div>
                                 </div>
-                                <div class="px-4 py-3 bg-gray-50 dark:bg-gray-700 text-right sm:px-6">
+                                <div class="px-4 py-3 bg-gray-50 dark:bg-gray-800 dark:border-t dark:border-gray-700 text-right sm:px-6">
                                     <x-button type="submit">Save</x-button>
                                 </div>
                             </div>
@@ -103,51 +68,50 @@
                     </section>
 
                     @if(count($invoices))
-                        <!-- Billing history -->
-                            <section aria-labelledby="billing-history-heading">
-                                <div class="bg-white dark:bg-gray-800 pt-6 shadow sm:rounded-md sm:overflow-hidden">
-                                    <div class="px-4 sm:px-6">
-                                        <h2 id="billing-history-heading" class="text-lg leading-6 font-medium text-gray-900 dark:text-gray-200">Billing history</h2>
-                                    </div>
-                                    <div class="mt-6 flex flex-col">
-                                        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                                            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                                                <div class="overflow-hidden border-t border-gray-200 dark:border-gray-600">
-                                                    <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-600">
-                                                        <thead class="bg-gray-50 dark:bg-gray-700">
-                                                        <tr>
-                                                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Date</th>
-                                                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Description</th>
-                                                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Amount</th>
-                                                            <!--
-                                                              `relative` is added here due to a weird bug in Safari that causes `sr-only` headings to introduce overflow on the body on mobile.
-                                                            -->
-                                                            <th scope="col" class="relative px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">
-                                                                <span class="sr-only">View receipt</span>
-                                                            </th>
-                                                        </tr>
-                                                        </thead>
-                                                        <tbody class="bg-white dark:bg-gray-700 divide-y divide-gray-200 dark:divide-gray-700">
-                                                            @foreach($invoices as $invoice)
-                                                                <tr>
-                                                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-200">
-                                                                        <time datetime="{{ $invoice->date()->format('Y-m-d') }}">{{ $invoice->date()->toFormattedDateString() }}</time>
-                                                                    </td>
-                                                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-200">{{ $invoice->lines->first()->description }}</td>
-                                                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-200">USD {{ format_money($invoice->amount_due) }}</td>
-                                                                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                                                        <a target="_blank" href="{{ $invoice->hosted_invoice_url }}" class="text-primary-600 hover:text-primary-900">View receipt</a>
-                                                                    </td>
-                                                                </tr>
-                                                            @endforeach
-                                                        </tbody>
-                                                    </table>
-                                                </div>
+                        <section aria-labelledby="billing-history-heading">
+                            <div class="bg-white dark:bg-gray-800 pt-6 shadow sm:rounded-md sm:overflow-hidden">
+                                <div class="px-4 sm:px-6">
+                                    <h2 id="billing-history-heading" class="text-lg leading-6 font-medium text-gray-900 dark:text-gray-200">Billing history</h2>
+                                </div>
+                                <div class="mt-6 flex flex-col">
+                                    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                                        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                            <div class="overflow-hidden border-t border-gray-200 dark:border-gray-700">
+                                                <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                                                    <thead class="bg-gray-50 dark:bg-gray-800">
+                                                    <tr>
+                                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Date</th>
+                                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Description</th>
+                                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Amount</th>
+                                                        <!--
+                                                          `relative` is added here due to a weird bug in Safari that causes `sr-only` headings to introduce overflow on the body on mobile.
+                                                        -->
+                                                        <th scope="col" class="relative px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">
+                                                            <span class="sr-only">View receipt</span>
+                                                        </th>
+                                                    </tr>
+                                                    </thead>
+                                                    <tbody class="bg-white dark:bg-gray-800 divide-y divide-gray-200 dark:divide-gray-700">
+                                                        @foreach($invoices as $invoice)
+                                                            <tr>
+                                                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-200">
+                                                                    <time datetime="{{ $invoice->date()->format('Y-m-d') }}">{{ $invoice->date()->toFormattedDateString() }}</time>
+                                                                </td>
+                                                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-200">{{ $invoice->lines->first()->description }}</td>
+                                                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-200">USD {{ format_money($invoice->amount_due) }}</td>
+                                                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                                    <a target="_blank" href="{{ $invoice->hosted_invoice_url }}" class="text-primary-600 hover:text-primary-900">View receipt</a>
+                                                                </td>
+                                                            </tr>
+                                                        @endforeach
+                                                    </tbody>
+                                                </table>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </section>
+                            </div>
+                        </section>
                     @endif
                 </div>
             </div>
