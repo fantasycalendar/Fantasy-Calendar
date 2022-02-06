7b3c990f63015869165514fc16ac1518bd95111a
V13Axel <alex@stechstudio.com>
2022-02-06 15:46:02 -0500

    Darken toggle

--- resources/views/components/input-toggle.blade.php
+++ resources/views/components/input-toggle.blade.php
@@ -8,7 +8,7 @@
         @endif
     </span>
 
-    <button @click="{{ $attributes->get('name') }} = !{{ $attributes->get('name') }}" type="button" :class="{'bg-gray-200': !{{ $attributes->get('name') }}, 'bg-primary-600': {{ $attributes->get('name') }}}" class="bg-gray-200 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500" role="switch" aria-checked="false" aria-labelledby="availability-label" aria-describedby="availability-description">
+    <button @click="{{ $attributes->get('name') }} = !{{ $attributes->get('name') }}" type="button" :class="{'bg-gray-200 dark:bg-gray-700': !{{ $attributes->get('name') }}, 'bg-primary-600': {{ $attributes->get('name') }}}" class="bg-gray-200 dark:bg-gray-700 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500" role="switch" aria-checked="false" aria-labelledby="availability-label" aria-describedby="availability-description">
         <span aria-hidden="true" :class="{'translate-x-0': !{{ $attributes->get('name') }}, 'translate-x-5': {{ $attributes->get('name') }}}" class="pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"></span>
     </button>
 </div>
