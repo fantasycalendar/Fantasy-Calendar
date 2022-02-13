9f526477109221cfef3f92d6bc693b2c6d846c96
V13Axel <alex@stechstudio.com>
2022-02-13 12:38:56 -0500

    Heading and logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,34 +1,48 @@
 <x-app-fullwidth-layout body-class="flex flex-col bg-gray-100 dark:bg-gray-900">
     <div class="flex-1 grid place-items-center">
-        <x-panel class="max-w-md w-full">
+        <div class="max-w-md w-full">
             <div class="grid">
-                <form method="POST" action="{{ route('login') }}" class="flex flex-col space-y-4">
-                    @csrf
-
-                    <div>
-                        <x-text-input name="identity" placeholder="{{ __('Username') }}" value="{{ old('username') ?? old('email') }}" error-on="username"></x-text-input>
+                <div>
+                    <div class="text-primary-700">
+                        <x-app-logo class="mx-auto h-12 w-auto"></x-app-logo>
                     </div>
+                    
+                    <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+                    <p class="mt-2 text-center text-sm text-gray-600">
+                        Or
+                        <x-app-link href="{{ route('register') }}"> register for free </x-app-link>
+                    </p>
+                </div>
 
-                    <div>
-                        <x-text-input placeholder="{{ __('Password') }}" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password"></x-text-input>
-                    </div>
+                <x-panel class="mt-8">
+                    <form method="POST" action="{{ route('login') }}" class="flex flex-col space-y-4">
+                        @csrf
 
-                    <x-input-toggle class="form-check-input" type="checkbox" name="remember" id="remember" label="{{ __('Remember Me') }}"></x-input-toggle>
+                        <div>
+                            <x-text-input name="identity" placeholder="{{ __('Username') }}" value="{{ old('username') ?? old('email') }}" error-on="username"></x-text-input>
+                        </div>
 
-                    <x-button id="login" type="submit" class="justify-center">
-                        {{ __('Login') }}
-                    </x-button>
+                        <div>
+                            <x-text-input placeholder="{{ __('Password') }}" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password"></x-text-input>
+                        </div>
 
-                    @if (Route::has('password.request'))
-                        <x-app-link href="{{ route('password.request') }}" class="text-sm text-center">
-                            {{ __('Forgot Your Password?') }}
-                        </x-app-link>
-                    @endif
-                </form>
+                        <x-input-toggle class="form-check-input" type="checkbox" name="remember" id="remember" label="{{ __('Remember Me') }}"></x-input-toggle>
+
+                        <x-button id="login" type="submit" class="justify-center">
+                            {{ __('Login') }}
+                        </x-button>
+
+                        @if (Route::has('password.request'))
+                            <x-app-link href="{{ route('password.request') }}" class="text-sm text-center underline">
+                                {{ __('Forgot Your Password?') }}
+                            </x-app-link>
+                        @endif
+                    </form>
+
+                    <x-slot name="footer"></x-slot>
+                </x-panel>
             </div>
-
-            <x-slot name="footer"></x-slot>
-        </x-panel>
+        </div>
 
     </div>
 </x-app-fullwidth-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -27,104 +27,67 @@
 	</script>
 @endpush
 
-<x-app-fullwidth-layout>
-    <div class="container pt-4" x-data='register_form()'>
+<x-app-fullwidth-layout body-class="flex flex-col bg-gray-100 dark:bg-gray-900">
+    <div class="flex-1 grid place-items-center" x-data='register_form()'>
         <div class="row justify-content-center">
-            <div class="col-lg-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Register') }}</div>
+            <x-panel>
+                <form method="POST" action="{{ route('register') }}" class="container-fluid">
+                    @csrf
+                    @honeypot
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('register') }}" class="container-fluid">
-                            @csrf
-                            @honeypot
-
-                            <div class="form-group row">
-                                <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" x-model='username' name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
-
-                                    @error('username')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" x-model='email' name="email" value="{{ old('email') ?? session('email') }}" required autocomplete="email">
-
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" x-model="password" name="password" required autocomplete="new-password" @blur="validate_password">
-
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password"
-                                        x-model="password_confirmation"
-                                        :class="{ 'is-invalid': password_was_validated && !password_valid }"
-                                        @keyup="validate_password"
-                                        @blur="validate_password">
-
-                                    <div class="invalid-feedback" x-show="password_was_validated && password.length < 7">Password must be 8 characters long.</div>
-
-                                    <div class="invalid-feedback" x-show="password_was_validated && password !== password_confirmation">Passwords do not match.</div>
-                                </div>
-                            </div>
-
-                            <input type='hidden' name='dark_theme' x-model='dark_theme'>
-
-                            <div class="form-check p-2">
-                                <input type="checkbox" class="form-check-input" name="policy_acceptance" id="policy_acceptance" x-model="agreed" required>
-                                <label class="form-check-label" for="policy_acceptance">I agree to the <a target="_blank" href="{{ route('terms-and-conditions') }}">Terms and Conditions</a>, and the <a target="_blank" href="{{ route('privacy-policy') }}">Privacy and Cookies Policy</a></label>
-                                <small>Residents of the EU are legally entitled to a 14-day cool off period, as explained in the T&Cs</small>
-                            </div>
-
-                            <div class="form-check p-2 mb-3">
-                                <input type="checkbox" class="form-check-input" name="marketing_acceptance" id="marketing_acceptance">
-                                <label class="form-check-label" for="marketing_acceptance">
-                                    <strong>(Optional)</strong> I would like to receive occasional emails about products and special offers
-                                    <small>(You can withdraw consent at any time on your profile)<small>
-                                </label>
-                            </div>
-
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" :disabled="username === '' || email === '' || !password_valid || !agreed" class="btn btn-primary">
-                                        {{ __('Register') }}
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
+                    <div class="col-md-6">
+                        <x-text-input id="username" type="text" class="form-control @error('username') is-invalid @enderror" x-model='username' name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
                     </div>
-                </div>
-            </div>
+
+                    <div class="col-md-6">
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" x-model='email' name="email" value="{{ old('email') ?? session('email') }}" required autocomplete="email">
+                    </div>
+
+                    <div class="col-md-6">
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" x-model="password" name="password" required autocomplete="new-password" @blur="validate_password">
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                        <div class="col-md-6">
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password"
+                                x-model="password_confirmation"
+                                :class="{ 'is-invalid': password_was_validated && !password_valid }"
+                                @keyup="validate_password"
+                                @blur="validate_password">
+
+                            <div class="invalid-feedback" x-show="password_was_validated && password.length < 7">Password must be 8 characters long.</div>
+
+                            <div class="invalid-feedback" x-show="password_was_validated && password !== password_confirmation">Passwords do not match.</div>
+                        </div>
+                    </div>
+
+                    <input type='hidden' name='dark_theme' x-model='dark_theme'>
+
+                    <div class="form-check p-2">
+                        <input type="checkbox" class="form-check-input" name="policy_acceptance" id="policy_acceptance" x-model="agreed" required>
+                        <label class="form-check-label" for="policy_acceptance">I agree to the <a target="_blank" href="{{ route('terms-and-conditions') }}">Terms and Conditions</a>, and the <a target="_blank" href="{{ route('privacy-policy') }}">Privacy and Cookies Policy</a></label>
+                        <small>Residents of the EU are legally entitled to a 14-day cool off period, as explained in the T&Cs</small>
+                    </div>
+
+                    <div class="form-check p-2 mb-3">
+                        <input type="checkbox" class="form-check-input" name="marketing_acceptance" id="marketing_acceptance">
+                        <label class="form-check-label" for="marketing_acceptance">
+                            <strong>(Optional)</strong> I would like to receive occasional emails about products and special offers
+                            <small>(You can withdraw consent at any time on your profile)<small>
+                        </label>
+                    </div>
+
+                    <div class="form-group row mb-0">
+                        <div class="col-md-6 offset-md-4">
+                            <button type="submit" :disabled="username === '' || email === '' || !password_valid || !agreed" class="btn btn-primary">
+                                {{ __('Register') }}
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </x-panel>
         </div>
     </div>
 </x-app-fullwidth-layout>
