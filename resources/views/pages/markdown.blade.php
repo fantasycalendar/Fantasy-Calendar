201e75fdec5b2f28d45c7ed243c5db4d8fa0f748
V13Axel <alex@stechstudio.com>
2022-02-16 08:09:40 -0500

    Center it --BETA

--- resources/views/pages/markdown.blade.php
+++ resources/views/pages/markdown.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="container markdown_container">
 
-        <div class="py-5 prose dark:prose-invert">
+        <div class="py-5 prose dark:prose-invert m-auto">
 
             <h1>{{ $title }}</h1>
             <p><i>Document Version {{ $version }}.0 — {{ $date }}</i></p>
