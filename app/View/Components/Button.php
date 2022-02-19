aae57b6e0fcc4641c85c9a6112179bc5943cc288
V13Axel <alex@stechstudio.com>
2022-02-18 23:33:46 -0500

    Like 98% of the way there

--- app/View/Components/Button.php
+++ app/View/Components/Button.php
@@ -20,6 +20,7 @@ class Button extends Component
         'primary' => 'text-white bg-primary-600 disabled:hover:bg-primary-600 hover:bg-primary-700 dark:bg-primary-900 disabled:dark:hover:bg-primary-900 dark:hover:bg-primary-800 focus:ring-primary-500 border-transparent shadow-sm',
         'secondary' => 'text-gray-700 bg-white disabled:hover:bg-white hover:bg-gray-50 focus:ring-primary-500 border-gray-300 dark:bg-gray-700 disabled:dark:hover:bg-gray-700 dark:text-gray-400 dark:hover:bg-gray-600 dark:border-gray-600 shadow-sm',
         'danger' => 'text-white bg-red-600 disabled:hover:bg-red-600 hover:bg-red-700 dark:bg-red-900 disabled:dark:hover:bg-red-900 dark:hover:bg-red-800 focus:ring-red-500 border-transparent shadow-sm',
+        'custom' => '',
     ];
 
     /**
--- resources/views/pages/discord.blade.php
+++ resources/views/pages/discord.blade.php
@@ -233,7 +233,7 @@
         <section class="grid place-items-center py-10" id="section3" style="background-color: #303136; background-image: url('{{ asset('resources/gray-square-bg.png') }}');">
             <div class="max-w-5xl my-5 py-5 grid sm:grid-cols-12 gap-10">
                 <div class="md:col-span-7 text-center md:text-left flex flex-col items-start justify-center">
-                    <h3 class="text-white text-3xl font-semibold">The information you need, just a command away.</h3>
+                    <h3 class="text-white text-2xl font-semibold">The information you need, just a command away.</h3>
                 </div>
 
                 <div class="md:col-span-5 text-center md:text-right">
@@ -245,125 +245,102 @@
                 </div>
 
                 <div class="md:col-span-7 text-center md:text-right flex flex-col md:items-end items-center justify-center">
-                    <h3 class="text-white text-3xl font-semibold">Quickly add or subtract minutes, hours, days, months, or years. <br><br>Thousands at once, even!</h3>
+                    <h3 class="text-white text-2xl font-semibold">Quickly add or subtract minutes, hours, days, months, or years. <br><br>Thousands at once, even!</h3>
+                </div>
+            </div>
+        </section>
+
+        <section class="py-16" style="position: relative; overflow: hidden; background-color: rgb(44, 47, 51);">
+            <div class="background z-10" style="background-image: url('{{ asset('resources/discord/webb-dark.png') }}'); position: absolute; top: 0; left: 0; right: 0; bottom: 0; opacity: 0.25; transform: scale(1.5, 1.5)"></div>
+            <div class="max-w-5xl mx-auto grid sm:grid-cols-2 py-5 z-20 relative gap-10">
+                <div class="text-center md:text-left flex flex-col md:items-start items-center justify-center text-white">
+                    <h3 class="text-white text-2xl font-semibold pb-8">Image or text, your choice.</h3>
+                    <h4 class="text-white text-xl">Prefer something you can paste into a text editor, wiki, or markdown page? The text-renderer will let you do just that.</h4>
+                </div>
+                <div class="text-center md:text-right flex flex-col sm:flex-row items-center sm:justify-end">
+                    <img src="{{ asset('resources/discord/discord_show_month_text.png') }}" style="min-height: 100%; box-shadow: 0 0 10px rgba(0, 0, 0, 0.2)">
                 </div>
             </div>
         </section>
 
         <section class="py-5" style="position: relative; overflow: hidden; background-color: rgb(44, 47, 51);">
-            <div class="background" style="background-image: url('{{ asset('resources/discord/webb-dark.png') }}'); position: absolute; top: 0; left: 0; right: 0; bottom: 0; opacity: 0.25; transform: scale(1.5, 1.5)"></div>
-            <div class=" py-5">
-                <div class="row">
-                    <div class="col-12 col-md-6 text-center text-md-left d-flex flex-column align-items-md-start align-items-center justify-content-center text-white">
-                        <h3>Image or text, your choice.</h3>
-                        <h4>Prefer something you can paste into a text editor, wiki, or markdown page? The text-renderer will let you do just that.</h4>
-                    </div>
-                    <div class="col-12 col-md-6 text-center text-md-right">
-                        <img src="{{ asset('resources/discord/discord_show_month_text.png') }}" style="box-shadow: 0 0 10px rgba(0, 0, 0, 0.2)">
-                    </div>
+            <div class="background z-10" style="background-image: url({{ asset('/resources/discord/gray-cubic-bg.png') }});position: absolute;top: 0;left: 0;right: 0;bottom: 0;opacity: 0.03;transform: scale(1.2);"></div>
+            <div class="max-w-5xl m-auto py-5 grid grid-cols-2 z-20 relative gap-10">
+                <div class="text-center md:text-left">
+                    <img src="{{ asset('resources/discord/detailed_day_info.png') }}" style="box-shadow: 0 0 10px rgba(0, 0, 0, 0.2)">
                 </div>
-            </div>
-        </section>
-
-        <section class="py-5" style="position: relative; overflow: hidden; background-color: rgb(44, 47, 51);">
-            <div class="background" style="background-image: url({{ asset('/resources/discord/gray-cubic-bg.png') }});position: absolute;top: 0;left: 0;right: 0;bottom: 0;opacity: 0.03;transform: scale(1.2);"></div>
-            <div class=" py-5">
-                <div class="row">
-                    <div class="col-12 col-md-6 text-center text-md-left">
-                        <img src="{{ asset('resources/discord/detailed_day_info.png') }}" style="box-shadow: 0 0 10px rgba(0, 0, 0, 0.2)">
-                    </div>
-                    <div class="col-12 col-md-6 text-center text-md-right d-flex flex-column align-items-md-end align-items-center justify-content-center text-white">
-                        <h3>Stats-nerds, we've got you covered.</h3>
-                        <h4>Quickly access detailed information about the current day with a simple command.</h4>
-                    </div>
+                <div class="text-center md:text-right flex flex-col md:items-end items-center justify-center text-white">
+                    <h3 class="text-white text-2xl font-semibold pb-8">Stats-nerds, we've got you covered.</h3>
+                    <h4 class="text-white text-xl">Quickly access detailed information about the current day with a simple command.</h4>
                 </div>
             </div>
         </section>
 
         <section class="py-5 darkmode" style="background-color: #222222; background-image: url('{{ asset('resources/gray-square-bg.png') }}');">
-            <div class=" py-5">
-                <div class="row">
-                    <div class="col-12 col-md-6 text-center text-md-left d-flex flex-column align-items-md-start align-items-center justify-content-center">
-                        <h3 style="color: white;">Privacy: Respected</h3>
-                        <h4 style="color: white;">
-                            We add new "slash commands" to your Discord server, <strong>not</strong> an always-listening bot.
-                            <br><br>
-                            Even if you use it in private channels, your messages stay yours.
-                        </h4>
-                    </div>
-                    <div class="col-12 col-md-6 text-center text-md-right">
-                        <img src="{{ asset('resources/discord/slash_commands.png') }}" style="box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);">
-                    </div>
+            <div class="max-w-5xl m-auto py-5 grid sm:grid-cols-2 gap-10">
+                <div class="text-center md:text-left flex flex-col md:items-start items-center justify-center">
+                    <h3 class="text-white text-2xl font-semibold pb-8">Privacy: Respected</h3>
+                    <h4 class="text-white text-xl">
+                        We add new "slash commands" to your Discord server, <strong>not</strong> an always-listening bot.
+                        <br><br>
+                        Even if you use it in private channels, your messages stay yours.
+                    </h4>
+                </div>
+                <div class="text-center md:text-right flex flex-col md:items-end items-center justify-center text-white">
+                    <img src="{{ asset('resources/discord/slash_commands.png') }}" style="box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);">
                 </div>
             </div>
         </section>
 
         <section class="py-5"  style="position: relative; overflow: hidden; background-color: rgb(44, 47, 51);">
-            <div class="background" style="background-image: url('{{ asset('resources/discord/double-bubble-dark.png') }}'); position: absolute; top: 0; left: 0; right: 0; bottom: 0; opacity: 0.15; transform: scale(1.5, 1.5)"></div>
-            <div class=" py-5 text-white">
-                <div class="row">
-                    <div class="col-12 col-md-6 text-center text-md-left">
-                        <iframe src="https://discord.com/widget?id=399974878134140939&theme=dark" width="350" height="400" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-                    </div>
-                    <div class="col-12 col-md-6 text-center text-md-right d-flex flex-column align-items-md-end align-items-center justify-content-center">
-                        <h3>Feedback Wanted!</h3>
-                        <h4>Confused? Something not working? Seeing an error message? Wish you could do/see/get something not currently available? Head over to our Discord server and let us know!</h4>
-                    </div>
+            <div class="background z-10" style="background-image: url('{{ asset('resources/discord/double-bubble-dark.png') }}'); position: absolute; top: 0; left: 0; right: 0; bottom: 0; opacity: 0.15; transform: scale(1.5, 1.5)"></div>
+            <div class="max-w-5xl m-auto py-5 grid sm:grid-cols-2 gap-10 text-white relative z-20">
+                <div class="col-12 col-md-6 text-center md:text-left">
+                    <iframe src="https://discord.com/widget?id=399974878134140939&theme=dark" width="350" height="400" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+                </div>
+                <div class="col-12 col-md-6 text-center md:text-right flex flex-col md:items-end items-center justify-center">
+                    <h3 class="text-white text-2xl font-semibold pb-8">Feedback Wanted!</h3>
+                    <h4 class="text-white text-xl">Confused? Something not working? Seeing an error message? Wish you could do/see/get something not currently available? Head over to our Discord server and let us know!</h4>
                 </div>
             </div>
         </section>
 
         <section class="py-5" style="position: relative; overflow: hidden; background-color: #222222;">
-            <div class="background" style="background-image: url('{{ asset('resources/discord/webb-dark.png') }}'); position: absolute; top: 0; left: 0; right: 0; bottom: 0; opacity: 0.15; transform: scale(1.5, 1.5)"></div>
-            <div class=" text-white" style="max-width: 1300px;">
-                <div class="row">
-                    <div class="col-12 text-center">
-                        <h2>Quick-Fire FAQs</h2>
-                    </div>
+            <div class="background z-10" style="background-image: url('{{ asset('resources/discord/webb-dark.png') }}'); position: absolute; top: 0; left: 0; right: 0; bottom: 0; opacity: 0.08; transform: scale(1.5, 1.5)"></div>
+            <div class="max-w-5xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8 relative z-20" style="max-width: 1300px;">
+                <div class="max-w-3xl mx-auto text-center">
+                    <h2 class="text-3xl font-extrabold text-gray-200">Quick-fire FAQs</h2>
+                    <p class="mt-4 text-lg text-gray-300">You have questions, we have answers.</p>
                 </div>
-                <div class="row pt-3 pb-3 justify-content-center">
-                    @include('pages.announcement._card', ['icon' => 'question-circle', 'title' => "How does it work?", 'content' => "You add the Fantasy Calendar integration to a Discord server you own or admin, then type <span style='font-family: monospace; border: 1px solid #6e6e6e; border-radius: 3px; padding: .2rem; font-size: .7rem;'>/fc</span> in that server. Easy as that! You can run <span style='font-family: monospace; border: 1px solid #6e6e6e; border-radius: 3px; padding: .2rem; font-size: .7rem;'>/fc help</span> for help."])
+                <dl class="mt-12 space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 lg:grid-cols-3 lg:gap-x-8">
+                    @include('pages.announcement._card', ['icon' => 'question-circle', 'title' => "How does it work?", 'content' => "You add the Fantasy Calendar integration to a Discord server you own or admin, then type <span class='bg-gray-900' style='font-family: monospace; border: 1px solid #6e6e6e; border-radius: 3px; padding: .2rem; font-size: .7rem;'>/fc</span> in that server. Easy as that! You can run <span class='bg-gray-900' style='font-family: monospace; border: 1px solid #6e6e6e; border-radius: 3px; padding: .2rem; font-size: .7rem;'>/fc help</span> for help."])
                     @include('pages.announcement._card', ['icon' => 'plug', 'title' => "Integration?", 'content' => "Yep! That just means a connection between our system and Discord's, to make life easier for you - now you can keep track of time right in Discord."])
                     @include('pages.announcement._card', ['icon' => 'dollar-sign', 'title' => "Why subscription-only?", 'content' => "In short: Every single /fc command costs us a small amount of money, adding up quickly. It also helps keep the lights on so we can keep making Fantasy-Calendar better!"])
-                </div>
-                <div class="row pb-1 justify-content-center">
                     @include('pages.announcement._card', ['icon' => 'user-secret', 'title' => "Does this give you my private messages?", 'content' => "<strong>Nope.</strong> No always-on bots in sight! All Discord sends us is the content of the commands you run, when you run them."])
-                    @include('pages.announcement._card', ['icon' => 'calendar-check', 'title' => "What can I do with it?", 'raw' => true, 'content' => "<ul class='text-left pl-4 m-auto' style='width: 90%;'><li>View the current date</li><li>Display the current month</li><li>Advance time and date</li><li>Share a direct calendar link</li><li>All directly from Discord</li></ul>"])
-                    @include('pages.announcement._card', ['icon' => 'newspaper', 'title' => "We still plan to add:", 'raw' => true, 'content' => "<ul class='text-left pl-4 pb-4 m-auto' style='width: 90%;'><li>Moons</li><li>Events+Categories</li><li>Weather</li><li>Clock</li><li>User management</li></ul>"])
-                </div>
+                    @include('pages.announcement._card', ['icon' => 'calendar-check', 'title' => "What can I do with it?", 'raw' => true, 'content' => "<ul class='text-left list-disc m-auto' style='width: 90%;'><li>View the current date</li><li>Display the current month</li><li>Advance time and date</li><li>Share a direct calendar link</li><li>All directly from Discord</li></ul>"])
+                    @include('pages.announcement._card', ['icon' => 'newspaper', 'title' => "We still plan to add:", 'raw' => true, 'content' => "<ul class='text-left list-disc pb-4 m-auto' style='width: 90%;'><li>Moons</li><li>Events+Categories</li><li>Weather</li><li>Clock</li><li>User management</li></ul>"])
+                </dl>
             </div>
         </section>
 
         <section class="py-3" style="background-color: #222222;">
-            <div class=" py-3">
-                <div class="row">
-                    <div class="col-12 text-center d-flex flex-column align-items-start justify-content-center">
-                        @if(!Auth::user())
-                        <h4 class='w-100'><x-app-link :href='route("subscription.pricing")' class='btn btn-lg btn-discord text-white mt-5 mb-2'>Register and Subscribe Now to Connect!</x-app-link></h4>
-                        @elseif(!Auth::user()->isPremium())
-                        <h4 class='w-100'><x-app-link :href='route("subscription.pricing")' class='btn btn-lg btn-discord text-white mt-5 mb-2'>Subscribe Now to Connect!</x-app-link></h4>
-                        @else
-                        <h4 class='w-100'><x-app-link :href='route("discord.index")' class='btn btn-lg btn-discord text-white mt-5 mb-2'>Connect Your Account Now!</x-app-link></h4>
-                        @endif
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12 text-center d-flex flex-column align-items-start justify-content-center">
-                        <h4 class='w-100'><x-app-link :href='route("discord.server")' class='btn btn btn-secondary text-white mb-5 mt-2'>Join Our Discord Server</x-app-link></h4>
-                    </div>
-                </div>
+            <div class="flex flex-col items-center py-10 space-y-4">
+                @if(!Auth::user())
+                <div><x-button-link role="custom" size="xl" class="text-white border-[color:#5865F2] dark:border-[color:#5865F2] bg-[#5865F2] dark:bg-[#5865F2] hover:bg-[color:#2f855a] dark:hover:border-[color:#2f855a] dark:hover:bg-[color:#2f855a] transition duration-100" :href='route("subscription.pricing")'>Register and Subscribe Now to Connect!</x-button-link></div>
+                @elseif(!Auth::user()->isPremium())
+                <div><x-button-link role="custom" size="xl" class="text-white border-[color:#5865F2] dark:border-[color:#5865F2] bg-[#5865F2] dark:bg-[#5865F2] hover:bg-[color:#2f855a] dark:hover:border-[color:#2f855a] dark:hover:bg-[color:#2f855a] transition duration-100" :href='route("subscription.pricing")'>Subscribe Now to Connect!</x-button-link></div>
+                @else
+                <div><x-button-link role="custom" size="xl" class="text-white border-[color:#5865F2] dark:border-[color:#5865F2] bg-[#5865F2] dark:bg-[#5865F2] hover:bg-[color:#2f855a] dark:hover:border-[color:#2f855a] dark:hover:bg-[color:#2f855a] transition duration-100" :href='route("discord.index")'>Connect Your Account Now!</x-button-link></div>
+                @endif
+                <div><x-button-link size="md" :href='route("discord.server")'>Join Our Discord Server</x-button-link></div>
             </div>
         </section>
 
-        <section class="py-5 discord-bg">
-            <section class="">
-                <div class="row">
-                    <div class="col-12 text-center text-white">
-                        <h3>With much more to come!</h3>
-                        <h4>We only plan to make it better.</h4>
-                    </div>
-                </div>
-            </section>
+        <section class="bg-[#5865F2] text-center">
+            <div class="h-full py-10">
+                <h1 class="text-white text-3xl mb-4 font-bold">With much more to come!</h1>
+                <h2 class="text-white text-xl font-semibold">We only plan to make it better.</h2>
+            </div>
         </section>
     </div>
 </x-app-fullwidth-layout>
