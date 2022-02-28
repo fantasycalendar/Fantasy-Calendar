2a7fbf479e4ee86735a7d293baabdd438e32f69d
V13Axel <alex@stechstudio.com>
2022-02-27 19:00:01 -0500

    Convert account deletion pages to tailwind

--- resources/views/pages/account-deletion-request.blade.php
+++ resources/views/pages/account-deletion-request.blade.php
@@ -1,60 +1,45 @@
-@extends('templates._page')
+<x-app-layout body-class="flex flex-col bg-gray-100 dark:bg-gray-900">
+    <x-panel class="max-w-3xl m-auto prose dark:prose-invert">
+            <h1>Account Deletion Request</h1>
+            <p>At Fantasy-Calendar, we take your privacy seriously. If you feel the need to delete your account, we will do so as soon as possible, typically within 14 days.</p>
+            <p class='mb-0'>We will delete all of the following on our servers, effectively removing it:</p>
 
-@section('content')
-    <div class="container w-50">
-        <div class="row py-5">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="card-text">
+            <ul>
+                <li>Username</li>
+                <li>Registration IP</li>
+                <li>All of your calendars</li>
+                <li>All event categories on your calendars</li>
+                <li>All events on your calendars</li>
+                <li>All comments on your events</li>
+                <li>All of your active calendar invitations</li>
+            </ul>
 
-                            <h3>Account Deletion Request</h3>
+            <p>In addition, any active subscriptions will be cancelled.</p>
 
-                            <p>At Fantasy-Calendar, we take your privacy seriously. If you feel the need to delete your account, we will do so as soon as possible, typically within 14 days.</p>
+            <p class='mb-0'>For financial reasons, we need to retain information regarding your past subscriptions:</p>
 
-                            <p class='mb-0'>We will delete all of the following on our servers, effectively removing it:</p>
+            <ul>
+                <li>Email Address</li>
+                <li>The last used card brand</li>
+                <li>The last four numbers of your card</li>
+            </ul>
 
-                            <ul>
-                                <li>Username</li>
-                                <li>Registration IP</li>
-                                <li>All of your calendars</li>
-                                <li>All event categories on your calendars</li>
-                                <li>All events on your calendars</li>
-                                <li>All comments on your events</li>
-                                <li>All of your active calendar invitations</li>
-                            </ul>
+            <hr>
 
-                            <p>In addition, any active subscriptions will be cancelled.</p>
+            <form action="/set-account-deletion" method="POST">
+                @csrf
 
-                            <p class='mb-0'>For financial reasons, we need to retain information regarding your past subscriptions:</p>
+                <x-text-input label="Please enter your password to verify your identity:" class="mb-4" required type="password" name="password"></x-text-input>
 
-                            <ul>
-                                <li>Email Address</li>
-                                <li>The last used card brand</li>
-                                <li>The last four numbers of your card</li>
-                            </ul>
+{{--                @error('password')--}}
+{{--                    <span class="invalid-feedback" role="alert">--}}
+{{--                        <strong>{{ $message }}</strong>--}}
+{{--                    </span>--}}
+{{--                @enderror--}}
 
-                            <hr>
+                <x-button role="danger" class="w-full justify-center" type="submit">Request Account Deletion</x-button>
 
-                            <form action="/set-account-deletion" method="POST">
-                                @csrf
-
-                                <label class="mt-2" for="password">Please enter your password to verify your identity:</label>
-                                <input class="form-control required mb-2 @error('password') is-invalid @enderror" required type="password" name="password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-
-                                <button type="submit" class="btn btn-danger w-100 text-center">Request Account Deletion</a>
-
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@endsection
+            </form>
+        <x-slot name="footer"></x-slot>
+    </x-panel>
+</x-app-layout>
