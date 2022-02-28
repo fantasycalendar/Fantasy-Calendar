2c24e4efa606ebbaec492ce5819fa321abde7b8a
V13Axel <alex@stechstudio.com>
2022-02-27 20:07:50 -0500

    Update email marketing subscription management views to tailwind

--- resources/views/pages/subscription-updated.blade.php
+++ resources/views/pages/subscription-updated.blade.php
@@ -1,20 +1,23 @@
 <x-app-layout body-class="flex flex-col bg-gray-100 dark:bg-gray-900">
-    <x-panel class="max-w-3xl m-auto prose dark:prose-invert">
-            <h4 class="text-center pt-2">
-                @if(Auth::user()->hasOptedInForMarketing())
-                    You're now subscribed to our marketing emails!
-                @else
-                    You have successfully unsubscribed from our marketing emails
-                @endif
-            </h4>
-        </div>
-        <div class="card-body text-center">
+    <x-panel class="max-w-3xl m-auto">
+        <x-alert type="success" class="text-xl">
             @if(Auth::user()->hasOptedInForMarketing())
-                <p>Don't worry, we won't spam you. We'll only send you updates and information we think you'll actually like.</p>
+                You're now subscribed to our marketing emails!
             @else
-                <p>If you didn't intend to do this, you can re-subscribe via your <a href="{{ route('profile') }}">user profile</a>.</p>
+                You have successfully unsubscribed from our marketing emails
             @endif
-        </div>
-        <x-slot name="footer"></x-slot>
+
+                <div>
+                    @if(Auth::user()->hasOptedInForMarketing())
+                        Don't worry, we won't spam you. We'll only send you updates and information we think you'll actually like.
+                    @else
+                        If you didn't intend to do this, you can re-subscribe via your <a href="{{ route('profile') }}">user profile</a>.
+                    @endif
+                </div>
+        </x-alert>
+
+        <x-slot name="footer_buttons">
+            <x-button-link href="{{ route('calendars.index') }}">Back to My Calendars</x-button-link>
+        </x-slot>
     </x-panel>
 </x-app-layout>
