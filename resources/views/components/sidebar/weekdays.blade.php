dd96909a934fa1360987861650a94bd46c29ceb7
V13Axel <alex@stechstudio.com>
2022-03-01 22:15:13 -0500

    Simplify, baby, simplify

--- resources/views/components/sidebar/weekdays.blade.php
+++ resources/views/components/sidebar/weekdays.blade.php
@@ -103,11 +103,10 @@
             </div>
         </div>
 
-        <div class='row no-gutters add_inputs global_week'>
-            <div class='col'>
-                <input type='text' class='form-control name' x-model="weekdayName" placeholder='Weekday name'>
-            </div>
-            <div class='col-auto'>
+        <div class='input-group'>
+            <input type='text' class='form-control name' x-model="weekdayName" placeholder='Weekday name'>
+
+            <div class='input-group-append mb-3'>
                 <button type='button' class='btn btn-primary add' @click="add(weekdayName)"><i class="fa fa-plus"></i></button>
             </div>
         </div>
@@ -117,16 +116,17 @@
                 <div class='sortable-container list-group-item'>
                     <div class='main-container' x-show="deleting !== weekday">
                         <div class='handle icon-reorder'></div>
-                        <div class='name-container'>
+                        <div class='name-container input-group'>
                             <input type='text' class='form-control name-input small-input' x-model="weekday"/>
+                            <div class="input-group-append">
+                                <div class='btn_remove btn btn-danger icon-trash' @click="deleting = weekday" x-show="deleting !== weekday"></div>
                             </div>
-                        <div class='remove-spacer'></div>
+                        </div>
                     </div>
 
                     <div class='remove-container'>
-                        <div class='remove-container-text' x-show="deleting === weekday">Are you sure you want to remove this?</div>
-                        <div class='btn_remove btn btn-danger icon-trash' @click="deleting = weekday" x-show="deleting !== weekday"></div>
                         <div class='btn_cancel btn btn-danger icon-remove' @click="deleting = null" x-show="deleting === weekday"></div>
+                        <div class='remove-container-text' x-show="deleting === weekday">Are you sure?</div>
                         <div class='btn_accept btn btn-success icon-ok' @click="remove(index)" x-show="deleting === weekday"></div>
                     </div>
                 </div>
