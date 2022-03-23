e4b7d9450af2e312ede654f4508fb120fe888e77
V13Axel <alex@stechstudio.com>
2022-03-22 23:38:21 -0400

    Support flashed messages on profile pages for appropriate redirects

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -19,6 +19,18 @@
                         </x-alert>
                     @endif
 
+                    @if(session()->has('error'))
+                        <x-alert type="danger">
+                            {{ session()->get('error') }}
+                        </x-alert>
+                    @endif
+
+                    @if(session()->has('message'))
+                        <x-alert type="success">
+                            {{ session()->get('message') }}
+                        </x-alert>
+                    @endif
+
                     {{ $slot }}
                 </div>
             </div>
