92d14596064c5320e16af3317b5a18d678897c8a
V13Axel <alex@stechstudio.com>
2022-07-21 21:32:13 -0400

    Migration to add fields to calendars

---
+++ database/migrations/2022_07_22_005158_add_real_time_fields_to_calendars.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('calendars', function (Blueprint $table) {
+            $table->boolean('advancement_enabled')->default(false);
+            $table->dateTime('advancement_next_due')->nullable();
+            $table->time('advancement_time')->nullable();
+            $table->string('advancement_timezone')->nullable();
+            $table->float('advancement_scale')->default(1);
+            $table->string('advancement_rate')->nullable();
+            $table->string('advancement_webhook_url')->nullable();
+            $table->string('advancement_webhook_format')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('calendars', function (Blueprint $table) {
+            $table->dropColumn([
+                'advancement_enabled',
+                'advancement_next_due',
+                'advancement_time',
+                'advancement_timezone',
+                'advancement_scale',
+                'advancement_rate',
+                'advancement_webhook_url',
+                'advancement_webhook_format',
+            ]);
+        });
+    }
+};
