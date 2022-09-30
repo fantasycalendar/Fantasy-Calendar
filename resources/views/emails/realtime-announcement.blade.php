90c146bfd3d808ec84feacba2d044b21261f050a
Haxxer <adamoresten@gmail.com>
2022-09-30 22:39:43 +0100

    beginnings of a marketing email

---
+++ resources/views/emails/realtime-announcement.blade.php
@@ -0,0 +1,25 @@
+@component('mail::marketing', ['user' => $user])
+# Fantasy Calendar now exists in realtime!
+
+Dear {{ $user->username }},
+
+We are excited to announce that we have created one of the most requested features thus far; Real-Time Advancement - your calendars can now be configured to automatically increment the date!
+
+@if($user->isPremium())
+It's quick to set up, easy to use, and available **right now** on all of your calendars!
+
+As always, thank you for your support through your subscription - Fantasy-Calendar is a passion project, and we could not have gotten this far without you!
+@else
+Quick to set up, easy to use, and available **right now** for subscribers. Only $2.49/month (or $24.99/year)!
+@endif
+
+@component('mail::button', ['url' => URL::route('discord')])
+    Check it out!
+@endcomponent
+
+Thanks,<br>
+The {{ config('app.name') }} team
+
+@endcomponent
+
+
