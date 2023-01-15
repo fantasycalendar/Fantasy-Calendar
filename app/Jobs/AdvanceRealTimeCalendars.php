c6263056d7d206ad905f1c35e1f575de0e88572c
V13Axel <alex@stechstudio.com>
2023-01-15 12:33:50 -0500

    Rate limiting

--- app/Jobs/AdvanceRealTimeCalendars.php
+++ app/Jobs/AdvanceRealTimeCalendars.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\Middleware\RateLimitedWithRedis;
 
 class AdvanceRealTimeCalendars implements ShouldQueue
 {
@@ -22,6 +23,11 @@ class AdvanceRealTimeCalendars implements ShouldQueue
     public function __construct()
     {
         //
+    }
+
+    public function middleware()
+    {
+        return [(new RateLimitedWithRedis('advancement'))->dontRelease()];
     }
 
     /**
