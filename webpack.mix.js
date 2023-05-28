74f9df3cce9ee9470744aa593e0bb02feab95e69
V13Axel <alex@stechstudios.com>
2023-05-27 23:25:31 -0400

    Update mix config to point to new docker container

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,79 +11,96 @@ const mix = require('laravel-mix');
  |
  */
 
-if(process.env.BROWSERSYNC === 'true') {
+if (process.env.BROWSERSYNC === "true") {
     mix.browserSync({
         injectChanges: true,
-        proxy: 'fc-bref-web:80',
+        proxy: "php:8000",
         port: 9980,
         open: false,
-        files: [
-            "**/*.css",
-            "**/*.php",
-            "**/*.md",
-            "**/*.js",
-            "**/*.vue",
-        ],
+        files: ["**/*.css", "**/*.php", "**/*.md", "**/*.js", "**/*.vue"],
         snippetOptions: {
             rule: {
                 match: /<\/body>/i,
                 fn: function(snippet, match) {
                     return snippet + match;
-                }
-            }
-        }
+                },
+            },
+        },
     });
 }
 
 // Copy components and vendor scripts
-mix.copyDirectory('resources/js/components', 'public/js/components');
-mix.copyDirectory('resources/js/vendor', 'public/js/vendor');
-mix.copyDirectory('resources/images', 'public/resources');
+mix.copyDirectory("resources/js/components", "public/js/components");
+mix.copyDirectory("resources/js/vendor", "public/js/vendor");
+mix.copyDirectory("resources/images", "public/resources");
 
 // Calendar scripts
-mix.copy('resources/js/calendar/header.js', 'public/js/calendar');
-mix.copy('resources/js/calendar/calendar_ajax_functions.js', 'public/js/calendar');
-mix.copy('resources/js/calendar/calendar_functions.js', 'public/js/calendar');
-mix.copy('resources/js/calendar/calendar_variables.js', 'public/js/calendar');
-mix.copy('resources/js/calendar/calendar_weather_layout.js', 'public/js/calendar');
-mix.copy('resources/js/calendar/calendar_day_data_layout.js', 'public/js/calendar');
-mix.copy('resources/js/calendar/calendar_season_generator.js', 'public/js/calendar');
-mix.copy('resources/js/calendar/calendar_inputs_visitor.js', 'public/js/calendar');
-mix.copy('resources/js/calendar/calendar_inputs_view.js', 'public/js/calendar');
-mix.copy('resources/js/calendar/calendar_inputs_edit.js', 'public/js/calendar');
-mix.copy('resources/js/calendar/calendar_manager.js', 'public/js/calendar');
-mix.copy('resources/js/calendar/calendar_presets.js', 'public/js/calendar');
-mix.copy('resources/js/calendar/calendar_workers.js', 'public/js/calendar');
+mix.copy("resources/js/calendar/header.js", "public/js/calendar");
+mix.copy(
+    "resources/js/calendar/calendar_ajax_functions.js",
+    "public/js/calendar"
+);
+mix.copy("resources/js/calendar/calendar_functions.js", "public/js/calendar");
+mix.copy("resources/js/calendar/calendar_variables.js", "public/js/calendar");
+mix.copy(
+    "resources/js/calendar/calendar_weather_layout.js",
+    "public/js/calendar"
+);
+mix.copy(
+    "resources/js/calendar/calendar_day_data_layout.js",
+    "public/js/calendar"
+);
+mix.copy(
+    "resources/js/calendar/calendar_season_generator.js",
+    "public/js/calendar"
+);
+mix.copy(
+    "resources/js/calendar/calendar_inputs_visitor.js",
+    "public/js/calendar"
+);
+mix.copy("resources/js/calendar/calendar_inputs_view.js", "public/js/calendar");
+mix.copy("resources/js/calendar/calendar_inputs_edit.js", "public/js/calendar");
+mix.copy("resources/js/calendar/calendar_manager.js", "public/js/calendar");
+mix.copy("resources/js/calendar/calendar_presets.js", "public/js/calendar");
+mix.copy("resources/js/calendar/calendar_workers.js", "public/js/calendar");
 
 // Webworkers
-mix.copy('resources/js/webworkers/worker_calendar.js', 'public/js/webworkers');
-mix.copy('resources/js/webworkers/worker_climate.js', 'public/js/webworkers');
-mix.copy('resources/js/webworkers/worker_events.js', 'public/js/webworkers');
-mix.copy('resources/js/webworkers/worker_event_tester.js', 'public/js/webworkers');
+mix.copy("resources/js/webworkers/worker_calendar.js", "public/js/webworkers");
+mix.copy("resources/js/webworkers/worker_climate.js", "public/js/webworkers");
+mix.copy("resources/js/webworkers/worker_events.js", "public/js/webworkers");
+mix.copy(
+    "resources/js/webworkers/worker_event_tester.js",
+    "public/js/webworkers"
+);
 
 // Misc
-mix.copy('resources/js/embed.js', 'public/js');
+mix.copy("resources/js/embed.js", "public/js");
 
 // Vendor
-mix.copy('node_modules/trumbowyg/dist/ui/icons.svg', 'public/images');
-mix.copy('node_modules/alpinejs/dist/cdn.js', 'public/js/vendor/alpine');
-mix.copy('node_modules/simplebar/dist/simplebar.min.js', 'public/js/vendor/simplebar');
-mix.copy('node_modules/simplebar/dist/simplebar.css', 'public/js/vendor/simplebar');
+mix.copy("node_modules/trumbowyg/dist/ui/icons.svg", "public/images");
+mix.copy("node_modules/alpinejs/dist/cdn.js", "public/js/vendor/alpine");
+mix.copy(
+    "node_modules/simplebar/dist/simplebar.min.js",
+    "public/js/vendor/simplebar"
+);
+mix.copy(
+    "node_modules/simplebar/dist/simplebar.css",
+    "public/js/vendor/simplebar"
+);
 
 // Compiled assets
-mix.js('resources/js/app.js', 'public/js')
+mix.js("resources/js/app.js", "public/js")
 
-    .sass('resources/sass/app.scss', 'public/css')
-    .sass('resources/sass/app-dark.scss', 'public/css');
+    .sass("resources/sass/app.scss", "public/css")
+    .sass("resources/sass/app-dark.scss", "public/css");
 
-mix.js('resources/js/app-tw.js', 'public/js');
+mix.js("resources/js/app-tw.js", "public/js");
 
 // require('mix-tailwindcss');
 
-mix.postCss('resources/sass/app-tw.css', 'public/css', [
+mix.postCss("resources/sass/app-tw.css", "public/css", [
     require("tailwindcss"),
 ]);
-
 
 if (mix.inProduction() || process.env.MIXVERSION) {
     mix.version();
