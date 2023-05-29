e4b043110d69f2e1be531db67802a214f91289d3
V13Axel <alex@stechstudio.com>
2023-05-29 00:18:26 -0400

    wip

--- app/Transformer/CalendarCoreTransformer.php
+++ app/Transformer/CalendarCoreTransformer.php
@@ -11,14 +11,57 @@ class CalendarCoreTransformer extends \League\Fractal\TransformerAbstract
 {
     public function transform(Calendar $calendar)
     {
-        return [
+        $value = [
             'name' => $calendar->name,
             'description' => $calendar->description ?? "",
 
-            'current_date' => $calendar->current_date,
-            'current_time' => $calendar->current_time,
+            'current_date' => $this->buildCurrentDate($calendar),
+            'current_time' => $this->buildCurrentTime($calendar),
 
-            'weekdays' => Arr::get($calendar->static_data, 'year_data.global_week'),
+            'weekdays' => $this->buildWeekdays($calendar),
+
+            'months' => $this->buildMonths($calendar),
         ];
+        // dd($value);
+        return $value;
+    }
+
+    private function buildCurrentDate(Calendar $calendar)
+    {
+        return [
+            "year" => $calendar->year,
+            "month" => [
+                "id" => $calendar->month->id
+            ],
+            "day" => $calendar->day,
+        ];
+    }
+
+    private function buildCurrentTime(Calendar $calendar)
+    {
+        $time = explode(':', $calendar->current_time);
+
+        return [
+            "hour" => intval($time[0]),
+            "minute" => intval($time[1]),
+        ];
+    }
+
+    private function buildWeekdays(Calendar $calendar)
+    {
+        return array_map(fn ($weekday) => ['name' => $weekday], Arr::get($calendar->static_data, 'year_data.global_week'));
+    }
+
+    private function buildMonths(Calendar $calendar): array
+    {
+        return $calendar->months->map(fn ($month) => [
+            'id' => $month->id,
+            'name' => $month->name,
+            'length' => $month->length,
+            'interval' => $month->interval,
+            'offset' => $month->offset,
+            'intercalary' => $month->type === 'intercalary',
+            'weekdays' => $month->week ?? [],
+        ])->toArray();
     }
 }
