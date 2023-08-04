e6af3e2b05500afb0b339b68848595436a9af915
V13Axel <alex@stechstudio.com>
2023-08-03 23:32:57 -0400

    Fix guest layout

--- resources/views/layouts/app-guest.blade.php
+++ resources/views/layouts/app-guest.blade.php
@@ -35,7 +35,7 @@
                     <button class="inline-flex items-center justify-center p-2 rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white" @click="menu = !menu">
                         <span class="sr-only">Open main menu</span>
                         <x-heroicon-o-bars-3 ::class="{ 'hidden': !menu, 'block': menu }" class="block h-6 w-6"></x-heroicon-o-bars-3>
-                        <x-heroicon-o-x ::class="{ 'hidden': menu, 'block': !menu }" class="hidden h-6 w-6"></x-heroicon-o-x>
+                        <x-heroicon-o-x-mark ::class="{ 'hidden': menu, 'block': !menu }" class="hidden h-6 w-6"></x-heroicon-o-x-mark>
                     </button>
                 </div>
             </div>
