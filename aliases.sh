5eb295c21e45c17e5b19a0f780d40cd1caf90252
V13Axel <alex@stechstudio.com>
2024-07-04 17:30:48 -0400

    Update configs to allow access to vite via docker

--- aliases.sh
+++ aliases.sh
@@ -2,10 +2,8 @@ alias npm='docker run -it -u $(id -u):$(id -g) -v "${PWD}/:/app" -w /app node:20
 alias npx='docker run -it -e AWS_PROFILE=fc -u $(id -u):$(id -g) -v "${PWD}/:/app" -v ~/.aws/:/home/node/.aws -w /app node:20 npx'
 alias node='docker run -it -u $(id -u):$(id -g) -v "${PWD}/:/app" -w /app node:20'
 alias artisan='docker compose exec php php artisan'
-alias fc_up='FC_WEB_PORT=9987 FC_BROWSERSYNC_PORT=9980 FC_BROWSERSYNC=true docker compose up'
-alias fc_nohot='docker compose up'
-alias fc_upd='FC_WEB_PORT=9987 FC_BROWSERSYNC_PORT=9980 FC_BROWSERSYNC=true docker compose up -d'
-alias fc_nohotd='docker compose up -d'
+alias fc_up='docker compose up'
+alias fc_upd='docker compose up -d'
 alias docker-clean='docker compose down && docker stop $(docker ps -a -q) && docker system prune -a -f && composer install && docker compose build'
 
 function composer() {
