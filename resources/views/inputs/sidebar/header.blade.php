b92898229579012d2b471fa26f22d302f13616b5
V13Axel <alex@stechstudio.com>
2024-07-18 23:32:25 -0400

    Fix sidebar heading via tailwind

--- resources/views/inputs/sidebar/header.blade.php
+++ resources/views/inputs/sidebar/header.blade.php
@@ -1,21 +1,14 @@
+<div class="bg-primary-700 flex justify-between items-center w-full h-[40px] px-3">
+    @auth
+        <a class="text-white font-semibold" href="{{ route('calendars.index') }}"><i class="fa fa-arrow-left"></i> My Calendars</a>
+    @endauth
+    @guest
+        <a class="text-white font-semibold" href="{{ route('home') }}"><i class="fa fa-arrow-left"></i> Back To Fantasy-Calendar</a>
+    @endguest
 
-<nav class="navbar-expand navbar-dark bg-accent sticky-top">
-    <div class="collapse navbar-collapse" id="collapsemenu">
-        <ul class="navbar-nav d-flex justify-content-between align-items-center w-100">
-            <li class="nav-item active">
-                @auth
-                    <a class="nav-link" href="{{ route('calendars.index') }}"><i class="fa fa-arrow-left"></i> My Calendars</a>
-                @endauth
-                @guest
-                    <a class="nav-link" href="{{ route('home') }}"><i class="fa fa-arrow-left"></i> Back To Fantasy-Calendar</a>
-                @endguest
-            </li>
-
-            <li class="nav-item" x-data="{ toggle() { window.toggle_sidebar(); } }">
-                <button class="nav-link btn py-0 mr-2" @click.prevent="toggle">
-                    <i class="fa fa-chevron-left"></i><i class="fa fa-chevron-left"></i>
-                </button>
-            </span>
-        </ul>
-    </div>
-</nav>
+    <span x-data="{ toggle() { window.toggle_sidebar(); } }">
+        <button class="text-white font-semibold py-0.5 px-1.5 rounded-sm hover:bg-primary-900/50" @click.prevent="toggle">
+            <i class="fa fa-chevron-left"></i><i class="fa fa-chevron-left"></i>
+        </button>
+    </span>
+</div>
