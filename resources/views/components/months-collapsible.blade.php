c7b09e2c6f8679bb4272392417db8634cc607053
V13Axel <alex@stechstudio.com>
2025-05-14 11:30:46 -0400

    Refinements to month inputs

--- resources/views/components/months-collapsible.blade.php
+++ resources/views/components/months-collapsible.blade.php
@@ -42,7 +42,7 @@
 
 @else
 
-    <div class='timespan row no-gutters input-group'>
+    <div class='timespan flex input-group'>
 
         <input type='text' class='form-control' placeholder='New month' x-model="name">
 
@@ -56,7 +56,7 @@
         </div>
     </div>
 
-    <div class="row no-gutters mb-2" x-show="months.length > 1">
+    <div class="flex mb-2" x-show="months.length > 1">
         <button class="full btn btn-secondary" @click="reordering = true" x-show="!reordering">
             <i class="fa fa-arrows-alt-v"></i> Change order
         </button>
@@ -72,7 +72,7 @@
     <div class="list-group mb-[1rem]" x-ref="months-sortable">
         <template x-for="(month, index) in months" :key="index" x-ref="months-sortable-template">
 
-            <div class="list-group-item px-1 py-0 first-of-type:rounded-t draggable-source" :data-id="index"
+            <div class="list-group-item px-2 py-0 !pt-0.5 !pb-0.5 first-of-type:rounded-t draggable-source" :data-id="index"
                  :class="month.type"
                  x-data="{ collapsed: true }">
 
@@ -83,14 +83,17 @@
                     </div>
                     <div class='cursor-pointer text-xl fa'
                          :class="{ 'fa-caret-square-up': !collapsed, 'fa-caret-square-down': collapsed }"
-                         @click="collapsed = !collapsed" x-show="!reordering"></div>
+                         @click="collapsed = !collapsed" x-show="!reordering">
+                    </div>
+
                     <div class="flex flex-grow-1 input-group">
                         <input x-model="month.name" type='text' class='flex-grow-1 w-auto form-control pr-0'/>
                         <input x-model="month.length" type='number' min='1' class='flex-shrink-1 form-control'/>
                     </div>
-                    <button class="btn btn-danger w-10" @click="deleting = index" x-show="!reordering">
-                        <i class="fa fa-trash text-lg"></i>
-                    </button>
+
+                    <div>
+                        <i class="fa fa-trash text-lg hover:text-red-400 hover:dark:text-red-600 cursor-pointer" @click="deleting = index" x-show="!reordering"></i>
+                    </div>
                 </div>
 
                 <div x-show="deleting === index" class="flex items-center w-full gap-x-2.5" x-cloak>
@@ -105,47 +108,35 @@
                     </button>
                 </div>
 
-                <div x-show="!collapsed && !reordering && deleting === -1">
-                    <div class='row no-gutters'>
-                        <div class='col'
-                             x-text="'Type: ' + (month.type === 'month' ? 'Month' : 'Intercalary month')"></div>
-                    </div>
+                <div class="flex flex-col px-2.5 py-2.5 space-y-2" x-show="!collapsed && !reordering && deleting === -1">
+                    <div x-text="'Intercalary: ' + (month.type === 'month' ? 'Yes' : 'No')"></div>
 
-                    <div class='row no-gutters mt-1'>
-                        <div class='col-6 pr-1'>
+                    <div class='flex flex-col'>
+                        <div class="grid grid-cols-2">
                             <div>Leap interval:</div>
-                        </div>
-
-                        <div class='col-6 pl-1'>
                             <div>Leap offset:</div>
                         </div>
-                    </div>
 
-                    <div class='row no-gutters mb-1'>
-                        <div class='col-6 pr-1'>
+                        <div class='input-group w-full'>
                             <input type='number' step="1" min='1' class='form-control small-input'
-                                   x-model.lazy.number="month.interval"/>
+                            x-model.lazy.number="month.interval"/>
+                            <input type='number' step="1" min='0' class='form-control small-input'
+                            x-model.lazy.number="month.offset" :disabled="month.interval === 1"/>
                         </div>
 
-                        <div class='col-6 pl-1'>
-                            <input type='number' step="1" min='0' class='form-control small-input'
-                                   x-model.lazy.number="month.offset" :disabled="month.interval === 1"/>
-                        </div>
                     </div>
 
-                    <div class='row no-gutters my-1'>
+                    <div class='flex'>
                         <div class='col-12 italics-text' x-text="getMonthIntervalText(month)"></div>
                     </div>
 
                     <div x-show="type === 'month'">
-                        <div class='row no-gutters my-1'>
-                            <div class='col-12'>
-                                <div class='separator'></div>
-                            </div>
+                        <div class='flex my-1'>
+                            <div class='separator'></div>
                         </div>
 
-                        <div class='row no-gutters my-1'>
-                            <div class='form-check col-12 py-2 border rounded'>
+                        <div class='flex my-1'>
+                            <div class='form-check w-full py-2 border rounded'>
                                 <input type='checkbox' :id='index + "_custom_week"'
                                        class='form-check-input'
                                        @click="toggleCustomWeek(month)"
@@ -160,13 +151,11 @@
                         <template x-if="month.week?.length">
                             <div class='custom-week-container'>
 
-                                <div class='row no-gutters my-1'>
-                                    <div class='col-12'>
-                                        Custom week length:
-                                    </div>
+                                <div class='flex my-1'>
+                                    Custom week length:
                                 </div>
 
-                                <div class='row no-gutters mb-1'>
+                                <div class='flex mb-1'>
                                     <div class='input-group'>
                                         <input type='number' min='1' step="1"
                                                class='form-control small-input'
@@ -182,8 +171,8 @@
                                     </div>
                                 </div>
 
-                                <div class='row no-gutters border'>
-                                    <div class='week_list col-12 p-1'>
+                                <div class='flex border'>
+                                    <div class='week_list w-full p-1'>
                                         <template x-for='(day, index) in (month.week ?? [])' :key='index'>
                                             <input x-model.lazy='month.week[index]' type='text'
                                                    class='form-control internal-list-name'/>
