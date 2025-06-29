85fc239dbc6e17583823915e6b686fa7155b7ba7
V13Axel <alex@stechstudio.com>
2025-06-28 21:24:43 -0400

    Mark it so

--- resources/views/inputs/full.blade.php
+++ resources/views/inputs/full.blade.php
@@ -70,7 +70,7 @@
 
         @if(request()->is('calendars/*/edit'))
             <x-collapsible :calendar="$calendar" contains="User Management" icon="fa-user"
-                           premium_feature="true" done></x-collapsible>
+                           premium_feature="true" polished></x-collapsible>
 
             <x-collapsible :calendar="$calendar" contains="Calendar Linking" icon="fa-link"
                            premium_feature="true" polished></x-collapsible>
