3c23a02cb134d8069d34c5c9f2971a112fc941a3
Haxxer <adamoresten@gmail.com>
2025-07-03 23:16:35 +0100

    SIIIMPLE

--- resources/views/templates/_calendar.blade.php
+++ resources/views/templates/_calendar.blade.php
@@ -4,21 +4,13 @@
     @include('templates._head_content')
 
     <body class="@guest dark @else @setting('dark_theme') dark @endsetting @endguest page-{{ str_replace('.', '-', Route::currentRouteName()) }} @stack('page-class')"
-          x-data="Viewport" @open-sidebar.window="open_sidebar" @close-sidebar.window="close_sidebar">
-        <div id="protip_container" class='d-print-none'></div>
+          x-data="Viewport"
+          @open-sidebar.window="open_sidebar"
+          @close-sidebar.window="close_sidebar">
 
         <div id="content">
             <x-loading-background></x-loading-background>
-
             @yield('content')
-
-            @env(['development'])
-                <script>
-                    // $(document).ready(function() {
-                    //     $.notify("This is the beta deployment of Fantasy Calendar. Use with caution.", {autoHide: false});
-                    // });
-                </script>
-            @endenv
         </div>
 
         <x-notification-area />
