1010927bdff6069c7a26e50a9395ce822d39825e
V13Axel <alex@stechstudio.com>
2026-03-11 10:07:08 -0400

    Derive Vite HMR host and port from .env

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,29 @@
-import {defineConfig} from "vite";
+import {defineConfig, loadEnv} from "vite";
 import laravel from "laravel-vite-plugin";
 
-export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        hmr: {
-            host: 'localhost',
-        }
-    },
-    plugins: [
-        laravel([
-            'resources/js/app.js',
-            'resources/js/app-tw.js',
-            'resources/sass/app.scss',
-            'resources/sass/app-dark.scss',
-            'resources/sass/app-tw.css',
-        ])
-    ],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    const appUrl = env.APP_URL || 'http://localhost:9980';
+    const hmrHost = new URL(appUrl).hostname;
+    const hmrPort = parseInt(env.FORWARD_VITE_PORT || '5173');
+
+    return {
+        server: {
+            host: '0.0.0.0',
+            cors: true,
+            hmr: {
+                host: hmrHost,
+                clientPort: hmrPort,
+            }
+        },
+        plugins: [
+            laravel([
+                'resources/js/app.js',
+                'resources/js/app-tw.js',
+                'resources/sass/app.scss',
+                'resources/sass/app-dark.scss',
+                'resources/sass/app-tw.css',
+            ])
+        ],
+    };
 });
