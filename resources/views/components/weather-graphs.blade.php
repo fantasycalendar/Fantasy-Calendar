e0a4db06bb520dba90f0f5918d1509e6498631af
V13Axel <alex@stechstudio.com>
2026-03-22 10:55:33 -0400

    Better climate graphs layout on bigger screens

--- resources/views/components/weather-graphs.blade.php
+++ resources/views/components/weather-graphs.blade.php
@@ -3,7 +3,7 @@
      @render-data-change.window='update_graphs'
      @open-sidebar.window='resize_graphs()'
      @close-sidebar.window='resize_graphs()'
-     class="space-y-12 px-4 py-6"
+     class="space-y-12 2xl:space-y-6 2xl:grid 2xl:grid-cols-2 2xl:gap-6 px-4 py-6"
 >
 
     <div x-show="day_length_graph_data.length">
@@ -16,7 +16,7 @@
         <canvas x-ref="temperature_canvas"></canvas>
     </div>
 
-    <div x-show="precipitation_graph_data.length">
+    <div x-show="precipitation_graph_data.length" class="2xl:col-span-2 2xl:w-1/2 2xl:justify-self-center">
         <h3 class='text-center mb-2 text-lg font-semibold'>Precipitation</h3>
         <canvas x-ref="precipitation_canvas"></canvas>
     </div>
