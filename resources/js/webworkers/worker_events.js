81a723ced511c2e1b69501e8b75f037cf570df92
V13Axel <alex@stechstudio.com>
2026-04-11 11:40:26 -0400

    Remove dead/merged workers

--- resources/js/webworkers/worker_events.js
+++
@@ -1,9 +0,0 @@
-import { event_evaluator } from "../calendar/calendar_workers";
-
-onmessage = e => {
-	data = event_evaluator.init(e.data.static_data, e.data.dynamic_data, e.data.events, e.data.event_categories, e.data.epoch_data, e.data.event_id, e.data.start_epoch, e.data.end_epoch, e.data.owner, e.data.callback);
-	postMessage({
-		event_data: data,
-		callback: false
-	});
-}
